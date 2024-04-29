171a68d859a3a0bb9df41cfda161646927656364
Donchev24 <booyaow24@gmail.com>
2024-04-29 22:19:30 +0100

    new test

---
+++ Programming Basics with CSharp/First Steps In Coding - Lab/test/new name/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _09._Yard_Greening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double totalSpace = double.Parse(Console.ReadLine());
+
+            double totalPrice = totalSpace * 7.61;
+
+            double discount = totalPrice * 0.18;
+
+            double finalPrice = totalPrice - discount;
+
+
+            Console.WriteLine($"The final price is: {finalPrice} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
