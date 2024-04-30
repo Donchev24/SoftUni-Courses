1dbef52a748d1437ed650807d41260789bc06d48
Donchev24 <booyaow24@gmail.com>
2024-04-30 20:47:33 +0100

    Added exercise

---
+++ Programming Basics with CSharp/First Steps In Coding - Exercise/03. Deposit Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _03._Deposit_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposit = double.Parse(Console.ReadLine());
+            int months = int.Parse(Console.ReadLine());
+            double YearlyProfit = double.Parse(Console.ReadLine());
+
+            double totalYearProfit = deposit * (YearlyProfit / 100);
+            double monthlyProfit = totalYearProfit / 12;
+
+            double finalAmount = deposit + months * monthlyProfit;
+            Console.WriteLine(finalAmount);
+        }
+    }
+}
