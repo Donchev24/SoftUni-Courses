ad3d0c01e4e00e771e09ef80503bb0fd5c3bcee2
Donchev24 <booyaow24@gmail.com>
2024-05-03 20:42:20 +0100

    Added Exercise

---
+++ Programming Basics with CSharp/Conditional Statements - Exercise/08. Lunch Break/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _08._Lunch_Break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string seriesName = Console.ReadLine();
+            int seriesTime = int.Parse(Console.ReadLine());
+            int breakTime = int.Parse(Console.ReadLine());
+
+            double timeForLunch = (double)breakTime * 1 / 8;
+            double timeForRelax = (double)breakTime * 1 / 4;
+            double timeLeft = breakTime - timeForLunch - timeForRelax;
+
+            if (timeLeft >= seriesTime)
+            {
+                Console.WriteLine($"You have enough time to watch {seriesName} and left with {Math.Ceiling(timeLeft - seriesTime)} minutes free time.");
+            }
+            else
+            {
+                Console.WriteLine($"You don't have enough time to watch {seriesName}, you need {Math.Ceiling(seriesTime - timeLeft)} more minutes.");
+            }
+        }
+    }
+}
