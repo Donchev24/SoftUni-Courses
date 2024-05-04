158ecd5ae654004d1b1729220fff97289fdcbbe8
Donchev24 <booyaow24@gmail.com>
2024-05-04 12:29:28 +0100

    Added Exercise

---
+++ Programming Basics with CSharp/For Loop - Lab/06. Vowels Sum/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _06._Vowels_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if ((char)text[i] == 'a')
+                    sum += 1;
+
+                else if ((char)text[i] == 'e')
+                    sum += 2;
+
+                else if ((char)text[i] == 'i')
+                    sum += 3;
+                else if ((char)text[i] == 'o')
+                    sum += 4;
+                else if ((char)text[i] == 'u')
+                    sum += 5;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
