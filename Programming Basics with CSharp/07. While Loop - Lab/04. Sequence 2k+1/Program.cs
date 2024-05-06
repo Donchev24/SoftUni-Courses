cd333663e21ec5c3f239792bd5e33229eff34aba
Donchev24 <booyaow24@gmail.com>
2024-05-06 18:52:24 +0100

    Added Exercise

---
+++ Programming Basics with CSharp/07. While Loop - Lab/04. Sequence 2k+1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _04._Sequence_2k_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 1;
+
+            while (sum <= number)
+            {
+                Console.WriteLine(sum);
+                sum = sum * 2 + 1;
+            }
+        }
+    }
+}
