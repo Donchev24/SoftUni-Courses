ef1f33be05b787ec128ddaf703e13ac867e31b21
Donchev24 <booyaow24@gmail.com>
2024-05-06 18:44:21 +0100

    Added Exercise

---
+++ Programming Basics with CSharp/06. For Loop - Exercise/03. Histogram/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _03._Histogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num;
+
+            int countP1 = 0;
+            int countP2 = 0;
+            int countP3 = 0;
+            int countP4 = 0;
+            int countP5 = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                num = int.Parse(Console.ReadLine());
+
+                if (num < 200)
+                    countP1++;
+                else if (num <= 399)
+                    countP2++;
+                else if (num <= 599)
+                    countP3++;
+                else if (num <= 799)
+                    countP4++;
+                else
+                    countP5++;
+
+            }
+
+            Console.WriteLine($"{(double)countP1 / n * 100:F2}%");
+            Console.WriteLine($"{(double)countP2 / n * 100:F2}%");
+            Console.WriteLine($"{(double)countP3 / n * 100:F2}%");
+            Console.WriteLine($"{(double)countP4 / n * 100:F2}%");
+            Console.WriteLine($"{(double)countP5 / n * 100:F2}%");
+        }
+    }
+}
