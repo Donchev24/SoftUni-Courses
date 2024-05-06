a8bd6c1aa683e01fd1ab6a1ab704028c9e70eda2
Donchev24 <booyaow24@gmail.com>
2024-05-06 18:52:41 +0100

    Added Exercise

---
+++ Programming Basics with CSharp/07. While Loop - Lab/05. Account Balance/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _05._Account_Balance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string imput = Console.ReadLine();
+
+            double totalMoney = 0;
+
+            while (imput != "NoMoreMoney")
+            {
+                double currentAmount = double.Parse(imput);
+
+                if (currentAmount < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                Console.WriteLine($"Increase: {currentAmount:F2}");
+                totalMoney += currentAmount;
+
+
+
+
+                imput = Console.ReadLine();
+
+            }
+
+            Console.WriteLine($"Total: {totalMoney:F2}");
+        }
+    }
+}
