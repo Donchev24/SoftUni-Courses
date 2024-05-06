80ca75a60d2bb2d20cabd4321cdc265a860bf90a
Donchev24 <booyaow24@gmail.com>
2024-05-06 18:52:05 +0100

    Added Exercise

---
+++ Programming Basics with CSharp/07. While Loop - Lab/03. Sum Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _03._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int imput;
+            int sum = 0;
+
+            while (sum < num)
+            {
+                imput = int.Parse(Console.ReadLine());
+                sum += imput;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
