417cc6f34ed29e76a4cbf1c51764e4d3cd2ebf81
Donchev24 <booyaow24@gmail.com>
2024-05-06 18:45:18 +0100

    Added Exercise

---
+++ Programming Basics with CSharp/06. For Loop - Exercise/06. Oscars/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _06._Oscars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string actorName = Console.ReadLine();
+            double points = double.Parse(Console.ReadLine());
+            int assessors = int.Parse(Console.ReadLine());
+
+            string assessorName;
+            double assessorScore = 0;
+
+
+            for (int i = 1; i <= assessors; i++)
+            {
+                assessorName = Console.ReadLine();
+                assessorScore = double.Parse(Console.ReadLine());
+
+                points += (assessorName.Length * assessorScore / 2);
+
+                if (points >= 1250.5)
+                {
+                    Console.WriteLine($"Congratulations, {actorName} got a nominee for leading role with {points:F1}!");
+                    break;
+                }
+            }
+
+            if (points < 1250.5)
+            {
+                Console.WriteLine($"Sorry, {actorName} you need {1250.5 - points:F1} more!");
+            }
+        }
+    }
+}
