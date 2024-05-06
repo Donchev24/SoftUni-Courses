43e86b8aa1dd9e10d0add258b07d3e48842bf95f
Donchev24 <booyaow24@gmail.com>
2024-05-06 18:45:02 +0100

    Added Exercise

---
+++ Programming Basics with CSharp/06. For Loop - Exercise/05. Salary/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _05._Salary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabs = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+            string website;
+
+            for (int i = 1; i <= tabs; i++)
+            {
+                website = Console.ReadLine();
+
+                if (website == "Facebook")
+                    salary -= 150;
+                else if (website == "Instagram")
+                    salary -= 100;
+                else if (website == "Reddit")
+                    salary -= 50;
+
+
+                if (salary <= 0)
+                {
+                    Console.WriteLine($"You have lost your salary.");
+                    break;
+                }
+
+            }
+
+            if (salary > 0)
+            {
+                Console.WriteLine(salary);
+            }
+        }
+    }
+}
