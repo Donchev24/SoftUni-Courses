f20d85652c12ab751c7dc2dd83f87a5f618df9bb
Donchev24 <booyaow24@gmail.com>
2024-05-06 18:44:43 +0100

    Added Exercise

---
+++ Programming Basics with CSharp/06. For Loop - Exercise/04. Clever Lily/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _04._Clever_Lily
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double washingMachine = double.Parse(Console.ReadLine());
+            int toyPrice = int.Parse(Console.ReadLine());
+
+            int savings = 0;
+            int toyCount = 0;
+
+            for (int i = 1; i <= age; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    savings += i * 5 - 1;
+                }
+                else
+                {
+                    toyCount++;
+                }
+            }
+
+            int totalMoney = savings + toyCount * toyPrice;
+
+            if (totalMoney >= washingMachine)
+            {
+                Console.WriteLine($"Yes! {totalMoney - washingMachine:F2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {Math.Abs(washingMachine - totalMoney):F2}");
+            }
+        }
+    }
+}
