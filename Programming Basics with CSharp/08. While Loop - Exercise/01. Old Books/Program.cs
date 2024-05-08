fdb67b27d0a18c2290d79d03e2c17bfc833e485a
Donchev24 <booyaow24@gmail.com>
2024-05-08 21:03:25 +0100

    Added Exercise

---
+++ Programming Basics with CSharp/08. While Loop - Exercise/01. Old Books/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01._Old_Books
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string book = Console.ReadLine();
+            int searchedBooks = 0;
+
+            while (true)
+            {
+                string search = Console.ReadLine();
+
+
+                if (book == search)
+                {
+                    Console.WriteLine($"You checked {searchedBooks} books and found it.");
+                    break;
+                }
+
+                if (search == "No More Books")
+                {
+                    Console.WriteLine($"The book you search is not here!");
+                    Console.WriteLine($"You checked {searchedBooks} books.");
+                    break;
+                }
+
+                searchedBooks++;
+            }
+        }
+    }
+}
