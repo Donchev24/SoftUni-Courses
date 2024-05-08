f4f8f983a17beed1837b04ed7bff5f0340ea69b0
Donchev24 <booyaow24@gmail.com>
2024-05-08 21:07:16 +0100

    Added Exercise

---
+++ Programming Basics with CSharp/08. While Loop - Exercise/03. Vacation/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace _03._Vacation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double tripCost = double.Parse(Console.ReadLine());
+            double startAmount = double.Parse(Console.ReadLine());
+
+            int days = 0;
+            int spendDays = 0;
+            double totalMoney = startAmount;
+
+            while (totalMoney < tripCost)
+            {
+
+
+                string action = Console.ReadLine();
+                double amount = double.Parse(Console.ReadLine());
+
+                days++;
+
+                if (action == "spend")
+                {
+                    spendDays++;
+
+                    if (spendDays == 5)
+                    {
+                        Console.WriteLine("You can't save the money.");
+                        Console.WriteLine(days);
+                        break;
+                    }
+
+                    if (amount >= totalMoney)
+                    {
+                        totalMoney = 0;
+                        continue;
+                    }
+
+                    totalMoney -= amount;
+
+                }
+
+                if (action == "save")
+                {
+                    totalMoney += amount;
+                    spendDays = 0;
+                }
+
+
+
+
+            }
+
+            if (totalMoney >= tripCost)
+            {
+                Console.WriteLine($"You saved the money for {days} days.");
+            }
+        }
+    }
+}
