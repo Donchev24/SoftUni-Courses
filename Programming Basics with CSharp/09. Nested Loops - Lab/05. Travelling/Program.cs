70d053feb82fd0b180219135f59c6d992d55e379
Donchev24 <booyaow24@gmail.com>
2024-05-08 21:42:11 +0100

    Added Exercise

---
+++ Programming Basics with CSharp/09. Nested Loops - Lab/05. Travelling/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _05._Travelling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+
+            while (destination != "End")
+            {
+                double budget = double.Parse(Console.ReadLine());
+                double savedMoney = 0.0;
+
+                while (savedMoney < budget)
+                {
+
+                    double newIncome = double.Parse(Console.ReadLine());
+                    savedMoney += newIncome;
+
+                    if (savedMoney >= budget)
+                    {
+                        Console.WriteLine($"Going to {destination}!");
+                        break;
+                    }
+                }
+
+                destination = Console.ReadLine();
+
+            }
+        }
+    }
+}
