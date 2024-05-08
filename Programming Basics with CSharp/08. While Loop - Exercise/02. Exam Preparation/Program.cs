ed03cbb87be2779ddeafebe4ec4d529dcc8d60cf
Donchev24 <booyaow24@gmail.com>
2024-05-08 21:06:59 +0100

    Added Exercise

---
+++ Programming Basics with CSharp/08. While Loop - Exercise/02. Exam Preparation/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _02._Exam_Preparation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int unhappyResults = int.Parse(Console.ReadLine());
+
+
+            int count = 0;
+            double totalScore = 0;
+            string exercisesName = Console.ReadLine();
+            int totalExercise = 0;
+            string lastExercise = string.Empty;
+
+            while (exercisesName != "Enough")
+            {
+
+                double grade = double.Parse(Console.ReadLine());
+                lastExercise = exercisesName;
+
+
+
+                if (grade <= 4)
+                {
+                    count++;
+                    if (count == unhappyResults)
+                    {
+                        Console.WriteLine($"You need a break, {count} poor grades.");
+                        break;
+                    }
+                }
+
+
+                totalScore += grade;
+                totalExercise++;
+
+
+
+
+
+                exercisesName = Console.ReadLine();
+            }
+
+
+            if (exercisesName == "Enough")
+            {
+                Console.WriteLine($"Average score: {totalScore / totalExercise:F2}");
+                Console.WriteLine($"Number of problems: {totalExercise}");
+                Console.WriteLine($"Last problem: {lastExercise}");
+            }
+
+        }
+    }
+}
