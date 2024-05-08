e289e42929c983aac66d0b18e23aceee4cac546d
Donchev24 <booyaow24@gmail.com>
2024-05-08 21:41:23 +0100

    Added Exercise

---
+++ Programming Basics with CSharp/09. Nested Loops - Lab/02. Multiplication Table/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _02._Multiplication_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i1 = 1; i1 <= 10; i1++)
+            {
+
+                for (int i2 = 1; i2 <= 10; i2++)
+                {
+                    int result = i1 * i2;
+                    Console.WriteLine($"{i1} * {i2} = {result}");
+                }
+            }
+        }
+    }
+}
