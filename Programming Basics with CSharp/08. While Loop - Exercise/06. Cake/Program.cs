c9c66bf5e22849c4c6f21bfe4654c0c0eedf0799
Donchev24 <booyaow24@gmail.com>
2024-05-08 21:12:09 +0100

    Added Exercise

---
+++ Programming Basics with CSharp/08. While Loop - Exercise/06. Cake/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _06._Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+
+            int totalCakes = length * width;
+
+            string input = Console.ReadLine();
+            int cakesTaken = 0;
+
+            while (input != "STOP")
+            {
+                cakesTaken = int.Parse(input);
+
+                totalCakes -= cakesTaken;
+
+                if (totalCakes <= 0)
+                {
+                    Console.WriteLine($"No more cake left! You need {Math.Abs(totalCakes)} pieces more.");
+                    break;
+                }
+
+
+
+                input = Console.ReadLine();
+            }
+
+
+            if (input == "STOP")
+            {
+                Console.WriteLine($"{totalCakes} pieces are left.");
+
+            }
+        }
+    }
+}
