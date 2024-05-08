09c8248b2763db745a434a0dd9a6721baeceb4ab
Donchev24 <booyaow24@gmail.com>
2024-05-08 21:42:27 +0100

    Added Exercise

---
+++ Programming Basics with CSharp/09. Nested Loops - Lab/06. Building/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _06._Building
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int floors = int.Parse(Console.ReadLine());
+            int rooms = int.Parse(Console.ReadLine());
+
+            for (int f = floors; f >= 1; f--)
+            {
+                if (f == floors)
+                {
+                    for (int l = 0; l < rooms; l++)
+                    {
+                        Console.Write($"L{f}{l} ");
+                    }
+
+                    Console.WriteLine();
+                }
+
+                else if (f % 2 != 0 && f != floors)
+                {
+                    for (int a = 0; a < rooms; a++)
+                    {
+                        Console.Write($"A{f}{a}" + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+                else if (f % 2 == 0 && f != floors)
+                {
+                    for (int o = 0; o < rooms; o++)
+                    {
+                        Console.Write($"O{f}{o}" + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
