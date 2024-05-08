5945b8c005d8590f1f03dea6b0f5d0f0d6e2844d
Donchev24 <booyaow24@gmail.com>
2024-05-08 21:12:27 +0100

    Added Exercise

---
+++ Programming Basics with CSharp/08. While Loop - Exercise/07. Moving/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _07._Moving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int higth = int.Parse(Console.ReadLine());
+
+            int totalSpace = width * length * higth;
+            string input = Console.ReadLine();
+            int spaceTaken = 0;
+
+            while (input != "Done")
+            {
+                spaceTaken = int.Parse(input);
+                totalSpace -= spaceTaken;
+
+                if (totalSpace <= 0)
+                {
+                    Console.WriteLine($"No more free space! You need {Math.Abs(totalSpace)} Cubic meters more.");
+                    break;
+                }
+
+
+
+                input = Console.ReadLine();
+            }
+
+
+            if (input == "Done")
+            {
+                Console.WriteLine($"{totalSpace} Cubic meters left.");
+            }
+        }
+    }
+}
