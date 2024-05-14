ccf886b724ed96364781ddc7522630b541d4654d
Donchev24 <booyaow24@gmail.com>
2024-05-14 21:40:29 +0100

    Added Exercise

---
+++ Programming Basics with CSharp/10. Nested Loops - Exercise/02. Equal Sums Even Odd Position/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _02._Equal_Sums_Even_Odd_Position
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            for (int i = num1; i <= num2; i++)
+            {
+
+                int odds = 0;
+                int evens = 0;
+
+                string currentNum = i.ToString();
+                for (int x = 0; x < currentNum.Length; x++)
+                {
+                    if (x % 2 == 0)
+                    {
+                        evens += currentNum[x];
+                    }
+                    else if (x % 2 != 0)
+                    {
+                        odds += currentNum[x];
+                    }
+                }
+                if (odds == evens)
+                {
+                    Console.Write($"{currentNum} ");
+                }
+            }
+        }
+    }
+}
