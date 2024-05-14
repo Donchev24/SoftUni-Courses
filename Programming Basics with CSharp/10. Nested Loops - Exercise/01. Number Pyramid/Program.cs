8098f3318ef483aac8f5b178e52bd1e2e66a0132
Donchev24 <booyaow24@gmail.com>
2024-05-14 21:40:10 +0100

    Added Exercise

---
+++ Programming Basics with CSharp/10. Nested Loops - Exercise/01. Number Pyramid/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _01._Number_Pyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int currentNum = 1;
+
+            bool isBigger = false;
+
+
+
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write($"{currentNum} ");
+                    currentNum++;
+                    if (currentNum > n)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+                }
+                if (isBigger)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
