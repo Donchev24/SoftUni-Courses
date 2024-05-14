238c211b720a994531d257a35a3b1652d0c25ce6
Donchev24 <booyaow24@gmail.com>
2024-05-14 21:41:48 +0100

    Added Exercise

---
+++ Programming Basics with CSharp/10. Nested Loops - Exercise/05. Special Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _05._Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    for (int y = 1; y <= 9; y++)
+                    {
+                        for (int x = 1; x <= 9; x++)
+                        {
+                            if (number % i == 0 && number % j == 0 && number % y == 0 && number % x == 0)
+
+                                Console.Write($"{i}{j}{y}{x} ");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
