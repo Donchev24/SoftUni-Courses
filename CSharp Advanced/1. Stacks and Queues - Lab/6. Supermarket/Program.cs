fec7d7236b3627021a7c86537348f7321ca8e767
Donchev24 <booyaow24@gmail.com>
2024-05-19 11:59:30 +0100

    Added Exercise

---
+++ CSharp Advanced/1. Stacks and Queues - Lab/6. Supermarket/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.Serialization;
+
+namespace _6._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            Queue<string> queue = new Queue<string>();
+
+            while ((input = Console.ReadLine())!= "End")
+            {
+
+                if (input == "Paid")
+                {
+                    int count = queue.Count;
+                    for (int i = 0; i < count; i++)
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                }
+                else
+                {
+
+                    queue.Enqueue(input);
+
+                }
+            }
+
+            Console.WriteLine($"{queue.Count} people remaining.");
+
+        }
+    }
+}
