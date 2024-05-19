f7d6cb265d365323bf7cf286edf1daf685675cb5
Donchev24 <booyaow24@gmail.com>
2024-05-19 11:59:53 +0100

    Added Exercise

---
+++ CSharp Advanced/1. Stacks and Queues - Lab/7. Hot Potato/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _7._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> childrenNames = new (
+                Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries));    //Alva James William   2
+
+
+            int hotPotato = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            while (childrenNames.Count > 1)
+            {
+                count++;
+
+                if (hotPotato == count)
+                {
+                    Console.WriteLine($"Removed {childrenNames.Dequeue()}");
+                    count = 0;
+                }
+                else
+                {
+                    childrenNames.Enqueue(childrenNames.Dequeue());
+                }
+
+                
+            }
+
+            Console.WriteLine($"Last is {childrenNames.Dequeue()}");
+        }
+    }
+}
