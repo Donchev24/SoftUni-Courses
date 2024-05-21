b17859f388e0b6123eca8b098657c8436913f1dd
Donchev24 <booyaow24@gmail.com>
2024-05-21 19:59:30 +0100

    Added Exercise

---
+++ CSharp Advanced/02.Sets and Dictionaries Advanced - Lab/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml;
+
+namespace _01.CountSameValuesInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] values = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, int> countOfValues = new();
+            
+            foreach (double value in values)
+            {
+                if (!countOfValues.ContainsKey(value))
+                {
+                    countOfValues.Add(value, 0);
+                }
+                countOfValues[value]++;
+            }
+
+           
+
+            foreach (KeyValuePair<double, int> number in countOfValues)
+            {
+                Console.WriteLine($"{number.Key} - {number.Value} times");
+            }
+
+           
+
+        }
+    }
+}
