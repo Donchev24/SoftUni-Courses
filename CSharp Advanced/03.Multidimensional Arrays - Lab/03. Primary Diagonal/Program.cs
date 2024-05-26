7c97f4efa11b7b9a21955d8afea03516551e7e01
Donchev24 <booyaow24@gmail.com>
2024-05-26 20:29:19 +0100

    Added Exercise

---
+++ CSharp Advanced/03.Multidimensional Arrays - Lab/03. Primary Diagonal/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _03._Primary_Diagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int rows = n;
+            int cols = n;
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] colValues = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colValues[col];
+                }
+            }
+
+            int sum = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                sum += matrix[i, i];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
