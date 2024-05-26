0f2a7c3b688d03576db520a72e1ef5fd1fac8553
Donchev24 <booyaow24@gmail.com>
2024-05-26 18:02:44 +0100

    Added Exercise

---
+++ CSharp Advanced/02.Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _06._Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string input = string.Empty;
+
+            HashSet<string> parkedCars = new();
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] parkingTokens = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string direction = parkingTokens[0];
+                string carNumber = parkingTokens[1];
+
+                if (parkingTokens[0] == "IN")
+                {
+                    if (!parkedCars.Contains(carNumber))
+                    {
+                        parkedCars.Add(carNumber);
+                    }
+                }
+
+                else
+                {
+                    if (parkedCars.Contains(carNumber))
+                    {
+                        parkedCars.Remove(carNumber);
+                    }
+                }
+            }
+
+            if (parkedCars.Any())
+            {
+                foreach (var car in parkedCars)
+                {
+                    Console.WriteLine($"{car}");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+
+        }
+     }
+  }
