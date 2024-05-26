bdb42a4e695baab7bffd4d6c6af00d66a4a92573
Donchev24 <booyaow24@gmail.com>
2024-05-26 18:09:38 +0100

    Renamed Folders

---
+++ CSharp Advanced/01. Stacks and Queues - Lab/02. Stack Sum/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.IO.Pipes;
+
+namespace _2._Stack_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> stack = new(input);
+            string command = string.Empty;
+
+
+            while ((command = Console.ReadLine().ToLower()) != "end")
+            {
+                string[] inputArgs = command.ToLower().Split();
+
+
+                if (inputArgs[0] == "add")
+                {
+                    int firstNumber = int.Parse(inputArgs[1]);
+                    int secondNumber = int.Parse(inputArgs[2]);
+
+                    stack.Push(firstNumber);
+                    stack.Push(secondNumber);
+                }
+
+                else if (inputArgs[0] == "remove")
+                {
+                    int removeCount = int.Parse(inputArgs[1]);
+
+                    if (removeCount <= stack.Count)
+                    {
+                        for (int i = 0; i < removeCount; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                }
+            }
+
+            if (stack.Any())
+            {
+                Console.WriteLine($"Sum: {stack.Sum()}");
+            }
+            else
+            {
+                Console.WriteLine($"Sum: 0");
+
+
+            }
+    }
+}
---
+++ CSharp Advanced/01. Stacks and Queues - Lab/03. Simple Calculator/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _3._Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // 2 + 5 + 10 - 2 - 1
+
+            Stack<string> stack = new (
+                Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Reverse()
+                );
+
+            int length = stack.Count;
+
+           
+
+
+            int sum = 0;
+            string currentAction = string.Empty;
+
+            for (int i = 0; i < length; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    if (i > 0)
+                    {
+                        if (currentAction == "+")
+                        {
+                            sum += int.Parse(stack.Pop());
+                        }
+                        else
+                        {
+                            sum -= int.Parse(stack.Pop());
+                        }
+                    }
+                    else
+                    {
+                        sum = int.Parse(stack.Pop());
+                    }
+                }
+                else
+                {
+                    currentAction = stack.Pop();
+                }
+
+
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
