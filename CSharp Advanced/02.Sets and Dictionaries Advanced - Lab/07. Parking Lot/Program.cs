ff1a37b498f0379d4781984bbb610269bb6d5af9
Donchev24 <booyaow24@gmail.com>
2024-05-26 18:03:05 +0100

    Added Exercise

---
+++ CSharp Advanced/02.Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _07._Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> uniqueNames = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                uniqueNames.Add(name);
+            }
+
+            foreach (var name in uniqueNames)
+            {
+                Console.WriteLine($"{name}");
+            }
+        }
+    }
+}
