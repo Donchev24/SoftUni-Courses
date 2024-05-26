62cbb562df0c6c27de7d2d8d092a604fab88d234
Donchev24 <booyaow24@gmail.com>
2024-05-26 20:29:59 +0100

    Added Exercise

---
+++ CSharp Advanced/03.Multidimensional Arrays - Lab/05. Square With Maximum Sum/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _05._Square_With_Maximum_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimentions = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = dimentions[0];
+            int cols = dimentions[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] values = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = values[col];
+                }
+            }
+            
+        }
+    }
+}
