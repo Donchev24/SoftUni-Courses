f4e972f43a841ddf330c1ba2fd0c4544c792e41d
Donchev24 <booyaow24@gmail.com>
2024-05-26 18:02:23 +0100

    Added Exercise

---
+++ CSharp Advanced/02.Sets and Dictionaries Advanced - Lab/05. CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+
+namespace _05._CitiesByContinentAndCountry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCities = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, List<string>>> citiesByContinentAndCountry = new();
+
+            for (int i = 0; i < numberOfCities; i++)
+            {
+                string[] dataTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string continent = dataTokens[0];
+                string country = dataTokens[1];
+                string city = dataTokens[2];
+
+                if (!citiesByContinentAndCountry.ContainsKey(continent))
+                {
+                    citiesByContinentAndCountry.Add(continent, new Dictionary<string, List<string>>());
+                    citiesByContinentAndCountry[continent].Add(country, new List<string>());
+                    citiesByContinentAndCountry[continent][country].Add(city);
+                }
+                else
+                {
+                    if (!citiesByContinentAndCountry[continent].ContainsKey(country))
+                    {
+                        citiesByContinentAndCountry[continent].Add(country, new List<string>());
+
+                    }
+                    citiesByContinentAndCountry[continent][country].Add(city);
+                }
+            }
+
+            foreach ((string continent, Dictionary<string, List<string>> countryOfCity) in citiesByContinentAndCountry)
+            {
+                Console.WriteLine($"{continent}: ");
+                foreach ((string country, List<string> city) in countryOfCity)
+                {
+                    Console.WriteLine($"{country} -> {string.Join(", ", city)}");
+                }
+            }
+
+            
+        }
+    }
+}
