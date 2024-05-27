3167f3f469ad3d93fa5605808e12eb4a5b07ed12
Donchev24 <booyaow24@gmail.com>
2024-05-27 15:29:56 +0100

    Added Exercise

---
+++ CSharp Advanced/03.Multidimensional Arrays - Lab/07. Pascal Triangle/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _07._Pascal_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            long[][] pascalTriangle = new long[size][];
+            pascalTriangle[0] = new long[1] { 1 };
+
+            for (int row = 1; row < pascalTriangle.Length; row++)
+            {
+                pascalTriangle[row] = new long[row + 1];
+
+                for (int col = 0; col < pascalTriangle[row].Length; col++)
+                {
+                    long sum = 0;
+
+                    if (col > 0)
+                    {
+                        sum += pascalTriangle[row - 1][col - 1];
+                    }
+                    if (col < pascalTriangle[row - 1].Length)
+                    {
+                        sum += pascalTriangle[row - 1][col];
+                    }
+
+                    pascalTriangle[row][col] = sum;
+                }
+            }
+
+            for (int row = 0; row < pascalTriangle.Length; row++)
+            {
+                for (int col = 0; col < pascalTriangle[row].Length; col++)
+                {
+                    Console.Write($"{pascalTriangle[row][col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
