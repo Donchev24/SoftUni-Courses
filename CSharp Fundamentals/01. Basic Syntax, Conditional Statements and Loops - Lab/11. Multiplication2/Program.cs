e847b8987af60c0873b20a495aa1a3e1d8e0dda0
Georgi Donchev <booyaow24@gmail.com>
2024-09-22 20:37:16 +0100

    Added Exercise

---
+++ CSharp Fundamentals/01. Basic Syntax, Conditional Statements and Loops - Lab/11. Multiplication2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _11._Multiplication2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int multiplier = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            do
+            {
+                for (int i = multiplier; ; i++)
+                {
+                    int result = number * i;
+
+                    if (i > 9)
+                    {
+                        Console.WriteLine($"{number} X {i} = {result}");
+                        break;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine($"{number} X {i} = {result}");
+                        counter++;
+                    }
+                }
+
+            } while (multiplier + counter <= 9);
+        }
+    }
+}
