1556c3aafd6e5395672023b6eb8ee54e8343ceda
Georgi Donchev <booyaow24@gmail.com>
2024-09-22 20:34:13 +0100

    Added Exercise

---
+++ CSharp Fundamentals/01. Basic Syntax, Conditional Statements and Loops - Lab/04. Back In 30 Min/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _04._Back_In_30_Min
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int totalMinutes = hour * 60 + minutes + 30;
+            int finalHour = totalMinutes / 60;
+            int finalMinutes = totalMinutes % 60;
+
+            if (finalHour > 23)
+            {
+                finalHour = 0;
+            }
+
+            Console.WriteLine($"{finalHour}:{finalMinutes:D2}");
+
+        }
+    }
+}
