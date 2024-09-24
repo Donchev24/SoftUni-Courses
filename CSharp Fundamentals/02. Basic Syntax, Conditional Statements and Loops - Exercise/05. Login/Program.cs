2781c1dc489fa3f96db47bdf7b6da5ca623bcd0e
Georgi Donchev <booyaow24@gmail.com>
2024-09-24 22:27:29 +0100

    Added Exercise

---
+++ CSharp Fundamentals/02. Basic Syntax, Conditional Statements and Loops - Exercise/05. Login/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _05._Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+
+            int length = username.Length;
+
+            string password = "";
+            int count = 0;
+
+            for (int i = length - 1; i >= 0; i--)
+            {
+                password += username[i];
+            }
+
+            string attempt = Console.ReadLine();
+
+            while (attempt != password)
+            {
+                count++;
+                if (count == 4)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    break;
+                }
+
+                Console.WriteLine("Incorrect password. Try again.");
+
+                attempt = Console.ReadLine();
+            }
+
+            if (attempt == password)
+            {
+                Console.WriteLine($"User {username} logged in.");
+            }
+        }
+    }
+}
