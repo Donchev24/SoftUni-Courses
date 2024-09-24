65366c796d95bfbac546a71f8e87661bb2ed688a
Georgi Donchev <booyaow24@gmail.com>
2024-09-24 22:25:11 +0100

    Added Exercise

---
+++ CSharp Fundamentals/11. Orders/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _11._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int orders = int.Parse(Console.ReadLine());
+            double totalPrice = 0;
+
+            for (int i = 1; i <= orders; i++)
+            {
+                double pricePerCapsule = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsulesCount = int.Parse(Console.ReadLine());
+
+                double price = ((days * capsulesCount) * pricePerCapsule);
+                totalPrice += price;
+
+                Console.WriteLine($"The price for the coffee is: ${price:F2}");
+
+                if (i == orders)
+                {
+                    Console.WriteLine($"Total: ${totalPrice:F2}");
+                }
+            }
+        }
+    }
+}
