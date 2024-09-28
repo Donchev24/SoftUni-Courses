da7dce7e3ce979fa837323cc032690852d932252
Georgi Donchev <booyaow24@gmail.com>
2024-09-28 17:58:55 +0100

    Added Exercise

---
+++ CSharp Fundamentals/03. Data Types and Variables - Lab/02. Pounds to Dollars/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _02._Pounds_to_Dollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float GBP = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{GBP * 1.31:F3}");
+        }
+    }
+}
