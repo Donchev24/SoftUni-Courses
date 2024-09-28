2c05887978d7742f6e1ff695b483c415006f266b
Georgi Donchev <booyaow24@gmail.com>
2024-09-28 17:59:44 +0100

    Added Exercise

---
+++ CSharp Fundamentals/03. Data Types and Variables - Lab/05. Special Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _05._Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int firstDigit = 0; ;
+            int lastDigit = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                firstDigit = i % 10;
+                lastDigit = i / 10;
+
+                int sum = firstDigit + lastDigit;
+
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+            }
+        }
+    }
+}
