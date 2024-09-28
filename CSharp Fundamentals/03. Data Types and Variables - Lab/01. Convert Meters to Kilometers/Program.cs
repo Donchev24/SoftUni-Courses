fbbe593ccf15525a781a9a4d6121324c2e5b92c4
Georgi Donchev <booyaow24@gmail.com>
2024-09-28 17:58:36 +0100

    Added Exercise

---
+++ CSharp Fundamentals/03. Data Types and Variables - Lab/01. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01._Convert_Meters_to_Kilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float metres = float.Parse(Console.ReadLine());
+        }
+    }
+}
