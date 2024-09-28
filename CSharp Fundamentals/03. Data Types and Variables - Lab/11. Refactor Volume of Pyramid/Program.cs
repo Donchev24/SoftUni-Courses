2808d3b86785225118ac13cd5edcfaeb813e3806
Georgi Donchev <booyaow24@gmail.com>
2024-09-28 18:01:17 +0100

    Added Exercise

---
+++ CSharp Fundamentals/03. Data Types and Variables - Lab/11. Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _11._Refactor_Volume_of_Pyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double highth = double.Parse(Console.ReadLine());
+            double V = (length * width * highth) / 3;
+
+            Console.Write($"Length: Width: Height: Pyramid Volume: {V:f2}");
+        }
+    }
+}
