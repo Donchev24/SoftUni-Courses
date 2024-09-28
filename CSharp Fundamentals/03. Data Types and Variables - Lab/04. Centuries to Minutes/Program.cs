042b2f3bd45b37d9f04276d5caa4cc1fb7f9014a
Georgi Donchev <booyaow24@gmail.com>
2024-09-28 17:59:30 +0100

    Added Exercise

---
+++ CSharp Fundamentals/03. Data Types and Variables - Lab/04. Centuries to Minutes/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _04._Centuries_to_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal centures = decimal.Parse(Console.ReadLine());
+
+            decimal years = centures * 100;
+            decimal days = Math.Round(years * (decimal)365.2422);
+            ulong hours = (ulong)days * 24;
+            ulong minutes = ((ulong)hours * 60);
+
+            Console.WriteLine($"{centures} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+        }
+    }
+}
