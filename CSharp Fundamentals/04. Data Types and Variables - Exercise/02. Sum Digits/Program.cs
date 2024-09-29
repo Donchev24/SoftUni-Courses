fd868f19dda7869973c804d3dc5785c50ec1b703
Georgi Donchev <booyaow24@gmail.com>
2024-09-29 13:48:37 +0100

    Added Exercise

---
+++ CSharp Fundamentals/04. Data Types and Variables - Exercise/02. Sum Digits/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _02._Sum_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int numCopy = num;
+            int result = 0;
+
+            while (numCopy > 0)
+            {
+                int lastDigitOfNum = numCopy % 10;
+                numCopy = numCopy / 10;
+                result += lastDigitOfNum;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
