e230727fd6025feff8613648dfd2605bda9b247f
Georgi Donchev <booyaow24@gmail.com>
2024-09-29 13:49:54 +0100

    Added Exercise

---
+++ CSharp Fundamentals/04. Data Types and Variables - Exercise/07. Water Overflow/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _07._Water_Overflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int tankCapacity = 255;
+
+            for (int i = 0; i < number; i++)
+            {
+                int inputLitres = int.Parse(Console.ReadLine());
+
+                if (inputLitres <= tankCapacity)
+                {
+                    tankCapacity -= inputLitres;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+            }
+
+            Console.WriteLine(255 - tankCapacity);
+        }
+    }
+}
