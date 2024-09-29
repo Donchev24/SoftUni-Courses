2b360cf893bb18d5a25773cd8c15f97477ffeae3
Georgi Donchev <booyaow24@gmail.com>
2024-09-29 13:50:36 +0100

    Added Exercise

---
+++ CSharp Fundamentals/04. Data Types and Variables - Exercise/10. Poke Mon/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _10._Poke_Mon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int Y = int.Parse(Console.ReadLine());
+
+            uint count = 0;
+            double halfOfN = N * 0.5d;
+
+            while (N >= M)
+            {
+                if (N == halfOfN && Y != 0)
+                {
+                    N /= Y;
+                }
+
+                if (N >= M)
+                {
+                    N -= M;
+                    count++;
+                }
+            }
+
+            Console.WriteLine(N);
+            Console.WriteLine(count);
+        }
+    }
+}
