8c0ce37327615c72db74d1d88b4ae29cacc00bbb
Georgi Donchev <booyaow24@gmail.com>
2024-09-29 18:02:16 +0100

    Added Exercise

---
+++ CSharp Fundamentals/05. Arrays - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _01._Day_of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            int num = int.Parse(Console.ReadLine());
+
+            if (num <= 7 && num >= 1)
+            {
+                Console.WriteLine(daysOfWeek[num - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
