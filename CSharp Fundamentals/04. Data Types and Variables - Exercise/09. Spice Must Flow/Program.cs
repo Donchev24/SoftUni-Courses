7183fec3d2a40b7ea50d2dcd1897160e03d15ecf
Georgi Donchev <booyaow24@gmail.com>
2024-09-29 13:50:22 +0100

    Added Exercise

---
+++ CSharp Fundamentals/04. Data Types and Variables - Exercise/09. Spice Must Flow/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _09._Spice_Must_Flow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int source = int.Parse(Console.ReadLine());
+
+            int sourceCopy = source;
+            int extratedSpice = 0;
+            int days = 0;
+
+            while (sourceCopy >= 100)
+            {
+                days++;
+                extratedSpice = extratedSpice + sourceCopy - 26;
+                sourceCopy -= 10;
+            }
+
+            if (extratedSpice >= 26)
+            {
+                extratedSpice -= 26;
+            }
+
+            Console.WriteLine(days);
+            Console.WriteLine(extratedSpice);
+        }
+    }
+}
