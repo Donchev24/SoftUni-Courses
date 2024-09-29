72d8529b026257088ab5de4e56d8b21bc06fe35d
Georgi Donchev <booyaow24@gmail.com>
2024-09-29 18:03:40 +0100

    Added Exercise

---
+++ CSharp Fundamentals/05. Arrays - Lab/07. Equal Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _07._Equal_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrFirst = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] arrSecond = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+
+            if (arrFirst.Length != arrSecond.Length)
+            {
+                return;
+            }
+
+            for (int i = 0; i < arrFirst.Length; i++)
+            {
+                if (arrFirst[i] != arrSecond[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    return;
+                }
+                sum += arrFirst[i];
+            }
+
+            Console.WriteLine($"Arrays are identical. Sum: {sum}");
+        }
+    }
+}
