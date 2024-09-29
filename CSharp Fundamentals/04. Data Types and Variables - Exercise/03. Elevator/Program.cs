7ac95d6030740bb395ea22553208e96c45db69d4
Georgi Donchev <booyaow24@gmail.com>
2024-09-29 13:48:57 +0100

    Added Exercise

---
+++ CSharp Fundamentals/04. Data Types and Variables - Exercise/03. Elevator/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _03._Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int persons = int.Parse(Console.ReadLine());
+            int personsLimit = int.Parse(Console.ReadLine());
+
+            int courses = 0;
+
+            if (persons % personsLimit == 0)
+            {
+                courses = persons / personsLimit;
+            }
+            else
+            {
+                courses = (persons / personsLimit) + 1;
+            }
+
+            Console.WriteLine(courses);
+        }
+    }
+}
