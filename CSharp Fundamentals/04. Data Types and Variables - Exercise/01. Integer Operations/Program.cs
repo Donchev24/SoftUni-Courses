0030bdf7260bc87bab787c969bd007a312c7ca16
Georgi Donchev <booyaow24@gmail.com>
2024-09-29 13:47:55 +0100

    Added Exercise

---
+++ CSharp Fundamentals/04. Data Types and Variables - Exercise/01. Integer Operations/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _01._Integer_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            int fourth = int.Parse(Console.ReadLine());
+
+            int firstExerc = first + second;
+            int secondExerc = firstExerc / third;
+            int thirdExerc = secondExerc * fourth;
+
+            Console.WriteLine(thirdExerc);
+        }
+    }
+}
