5a0b7276dd80790ef84bfff32e4f61c3563cf51d
Georgi Donchev <booyaow24@gmail.com>
2024-09-29 13:50:08 +0100

    Added Exercise

---
+++ CSharp Fundamentals/04. Data Types and Variables - Exercise/08. Beer Kegs/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _08._Beer_Kegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            string biggestModel = "";
+            double biggestVolume = double.MinValue;
+            for (int i = 0; i < num; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int hight = int.Parse(Console.ReadLine());
+
+                double volume = Math.PI * radius * radius * hight;
+
+                if (volume >= biggestVolume)
+                {
+                    biggestVolume = volume;
+                    biggestModel = model;
+                }
+            }
+
+            Console.WriteLine(biggestModel);
+        }
+    }
+}
