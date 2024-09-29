01afb5ad6e270861c8f8703c80874200821c043f
Georgi Donchev <booyaow24@gmail.com>
2024-09-29 13:49:26 +0100

    Added Exercise

---
+++ CSharp Fundamentals/04. Data Types and Variables - Exercise/05. Print Part Of ASCII Table/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _05._Print_Part_Of_ASCII_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int unicodeStart = int.Parse(Console.ReadLine());
+            int unicodeEnd = int.Parse(Console.ReadLine());
+
+            for (int i = unicodeStart; i <= unicodeEnd; i++)
+            {
+                char currentChar = (char)i;
+                Console.Write($"{currentChar} ");
+            }
+        }
+    }
+}
