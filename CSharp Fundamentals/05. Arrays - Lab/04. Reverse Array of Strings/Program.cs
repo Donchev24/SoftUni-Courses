9a3c0b61e1de0c257ae213063f821fe6fbd8dbb2
Georgi Donchev <booyaow24@gmail.com>
2024-09-29 18:02:59 +0100

    Added Exercise

---
+++ CSharp Fundamentals/05. Arrays - Lab/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _04._Reverse_Array_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                string firstElement = input[i];
+                string lastElement = input[input.Length - 1 - i];
+
+                input[i] = lastElement;
+                input[input.Length - 1 - i] = firstElement;
+            }
+
+            Console.WriteLine(string.Join(" ", input));
+        }
+    }
+}
