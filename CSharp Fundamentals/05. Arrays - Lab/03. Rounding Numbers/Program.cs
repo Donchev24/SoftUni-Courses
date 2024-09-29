31e3540e7b8773c1000918a57e055b9e751a0bac
Georgi Donchev <booyaow24@gmail.com>
2024-09-29 18:02:47 +0100

    Added Exercise

---
+++ CSharp Fundamentals/05. Arrays - Lab/03. Rounding Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _03._Rounding_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arrOfNums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            int[] roundedNums = new int[arrOfNums.Length];
+
+            for (int i = 0; i < arrOfNums.Length; i++)
+            {
+                roundedNums[i] = (int)Math.Round(arrOfNums[i], MidpointRounding.AwayFromZero);
+            }
+
+            for (int i = 0; i < arrOfNums.Length; i++)
+            {
+                Console.WriteLine($"{arrOfNums[i]} => {roundedNums[i]}");
+            }
+        }
+    }
+}
