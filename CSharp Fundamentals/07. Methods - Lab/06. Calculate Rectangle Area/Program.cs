5bb2ee8f0f259682c63c8c6d20860fff0058f10a
Georgi Donchev <booyaow24@gmail.com>
2024-10-04 22:11:19 +0100

    Added Exercise

---
+++ CSharp Fundamentals/07. Methods - Lab/06. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _06._Calculate_Rectangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double result = 0;
+
+            Console.WriteLine(RectangleArea(a, b, result));
+        }
+
+        static double RectangleArea(double a, double b, double result)
+        {
+            result = a * b;
+            return result;
+        }
+    }
+}
