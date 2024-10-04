6168920ed47d04c4d5b9bb89818ad2be1168f2c4
Georgi Donchev <booyaow24@gmail.com>
2024-10-04 21:53:29 +0100

    Added Exercise

---
+++ CSharp Fundamentals/06. Arrays - Exercise/06. Equal Sum/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _06._Equal_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int mainIndex = 0; mainIndex < nums.Length; mainIndex++) 
+            {
+                int backwardsSum = 0;
+                int forwardSum = 0;
+
+                for (int backwards = nums.Length - 1; backwards > mainIndex; backwards--)
+                {
+                    backwardsSum += nums[backwards];
+                }
+
+                for (int forward = 0; forward < mainIndex; forward++)
+                {
+                    forwardSum += nums[forward];
+                }
+
+
+                if (forwardSum == backwardsSum)
+                {
+                    Console.WriteLine(mainIndex);
+                    return;
+                }
+            }
+
+            Console.WriteLine("no");
+        }
+    }
+}
