1d1c0f564046935ec57dcf473f111aca6dc38879
Georgi Donchev <booyaow24@gmail.com>
2024-10-04 21:53:15 +0100

    Added Exercise

---
+++ CSharp Fundamentals/06. Arrays - Exercise/05. Top Integers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _05._Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int y = 0; y < arr.Length; y++)
+            {
+                bool isBigger = false;
+
+                for (int i = y + 1; i < arr.Length; i++) 
+                {
+                    if (arr[y] > arr[i])
+                    {
+                        isBigger = true;
+                    }
+                    else
+                    {
+                        isBigger = false;
+                        break;
+                    }
+                }
+
+                if (isBigger)
+                {
+                    Console.Write($"{arr[y]} ");
+                }
+            }
+
+            Console.Write(arr[arr.Length - 1]);
+        }
+    }
+}
