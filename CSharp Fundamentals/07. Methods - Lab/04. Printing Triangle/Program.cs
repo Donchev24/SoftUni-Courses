dff5c165c14c4d8f68f3f01136f254c8f9f1571d
Georgi Donchev <booyaow24@gmail.com>
2024-10-04 22:10:58 +0100

    Added Exercise

---
+++ CSharp Fundamentals/07. Methods - Lab/04. Printing Triangle/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _04._Printing_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            PyramidOfNumbers(n);
+        }
+
+        static void PyramidOfNumbers(int number)
+        {
+            for (int row = 1; row <= number; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write($"{col} ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int row = number - 1; row >= 1; row--)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write($"{col} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
