9595fd00af26bb0be212781de797838da8c7e8e1
Georgi Donchev <booyaow24@gmail.com>
2024-10-04 21:52:50 +0100

    Added Exercise

---
+++ CSharp Fundamentals/06. Arrays - Exercise/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _03._Zig_Zag_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] firstArr = new int[n];
+            int[] secondArr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] incomingInts = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                if (i % 2 == 0)
+                {
+                    firstArr[i] = incomingInts[0];
+                    secondArr[i] = incomingInts[1];
+                }
+                else
+                {
+                    secondArr[i] = incomingInts[0];
+                    firstArr[i] = incomingInts[1];
+                }
+            }
+
+            for (int i = 0; i < firstArr.Length; i++)
+            {
+                Console.Write($"{firstArr[i]} ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < secondArr.Length; i++)
+            {
+                Console.Write($"{secondArr[i]} ");
+            }
+        }
+    }
+}
