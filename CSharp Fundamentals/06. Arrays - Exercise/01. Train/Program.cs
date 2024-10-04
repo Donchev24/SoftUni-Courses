935f4d6d97bd69e7b205e7ca72d8574ff24bca7e
Georgi Donchev <booyaow24@gmail.com>
2024-10-04 21:52:18 +0100

    Added Exercise

---
+++ CSharp Fundamentals/06. Arrays - Exercise/01. Train/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] waggons = new int[n];
+
+            int sum = 0;
+
+            for (int i = 0; i < waggons.Length; i++)
+            {
+                waggons[i] = int.Parse(Console.ReadLine());
+                sum += waggons[i];
+            }
+
+            for (int i = 0; i < waggons.Length; i++)
+            {
+                Console.Write($"{waggons[i]} ");
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(sum);
+        }
+    }
+}
