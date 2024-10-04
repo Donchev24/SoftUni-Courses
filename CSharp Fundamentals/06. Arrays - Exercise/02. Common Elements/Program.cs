0bfe2a4912952a54a2fe90709a667a345fb34ed2
Georgi Donchev <booyaow24@gmail.com>
2024-10-04 21:52:38 +0100

    Added Exercise

---
+++ CSharp Fundamentals/06. Arrays - Exercise/02. Common Elements/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _02._Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstText = Console.ReadLine().Split();
+            string[] secondText = Console.ReadLine().Split();
+
+            for (int i = 0; i < secondText.Length; i++)
+            {
+                for (int y = 0; y < firstText.Length; y++)
+                {
+                    if (secondText[i] == firstText[y])
+                    {
+                        Console.Write($"{secondText[i]} ");
+                    }
+                }
+            }
+        }
+    }
+}
