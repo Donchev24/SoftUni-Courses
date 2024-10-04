decb686ff0bd23bec6e9eda860bb4d6f8665eac6
Georgi Donchev <booyaow24@gmail.com>
2024-10-04 21:53:40 +0100

    Added Exercise

---
+++ CSharp Fundamentals/06. Arrays - Exercise/08. Magic Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _08._Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();    //1 7 6 2 19 23
+
+            int number = int.Parse(Console.ReadLine());
+
+            for (int mainIndex = 0; mainIndex < arr.Length; mainIndex++)
+            {
+                for (int i = mainIndex + 1; i < arr.Length; i++)
+                {
+                    if (arr[mainIndex] + arr[i] == number)
+                    {
+                        Console.WriteLine($"{arr[mainIndex]} {arr[i]}");
+
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
