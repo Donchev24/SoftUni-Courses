d54f062f5e7c471a27cf67e8c85d02b25d6c5081
Georgi Donchev <booyaow24@gmail.com>
2024-10-04 22:10:47 +0100

    Added Exercise

---
+++ CSharp Fundamentals/07. Methods - Lab/03. Calculations/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _03._Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string mathAction = Console.ReadLine();
+            double firstNumber = double.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            ResultOfCalculations(mathAction, firstNumber, secondNumber);
+        }
+
+        static void ResultOfCalculations(string action, double firstNumber, double SecondNumber)
+        {
+            double result = 0;
+            if (action == "add")
+            {
+                result = firstNumber + SecondNumber;
+            }
+            else if (action == "multiply")
+            {
+                result = firstNumber * SecondNumber;
+            }
+            else if (action == "subtract")
+            {
+                result = firstNumber - SecondNumber;
+            }
+            else
+            {
+                result = firstNumber / SecondNumber;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
