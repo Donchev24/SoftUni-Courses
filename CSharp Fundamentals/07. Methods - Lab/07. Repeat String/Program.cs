a7dfb2e3fb32d355f7ba991ccd0770e42a9e9fe0
Georgi Donchev <booyaow24@gmail.com>
2024-10-04 22:11:33 +0100

    Added Exercise

---
+++ CSharp Fundamentals/07. Methods - Lab/07. Repeat String/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace _07._Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int repeats = int.Parse(Console.ReadLine());
+            string result = "";
+
+            Console.Write(StringRepeat(input, repeats, result));
+        }
+
+        static string StringRepeat(string input, int repeats, string result)
+        {
+            StringBuilder TEST = new StringBuilder();
+            result = input;
+
+            for (int i = 0; i < repeats; i++)
+            {
+                TEST.Append(result);
+            }
+
+            return TEST.ToString();
+        }
+    }
+}
