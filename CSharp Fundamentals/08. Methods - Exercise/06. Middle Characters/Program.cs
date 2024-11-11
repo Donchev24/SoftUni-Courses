36d9b00cc97d281f2a82ae55fdd2401cf85d4bb7
Georgi Donchev <booyaow24@gmail.com>
2024-11-11 19:44:00 +0000

    Added Exercise

---
+++ CSharp Fundamentals/08. Methods - Exercise/06. Middle Characters/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _06._Middle_Characters
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string str = Console.ReadLine();
+            int a = str.Length % 2;
+            char middleChar = str[str.Length / 2];
+            Console.WriteLine(middleChar);
+        }
+    }
+}
