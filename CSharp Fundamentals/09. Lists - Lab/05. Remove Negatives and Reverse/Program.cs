84c77bf8e175f9105be17c11fa7f599106d56c81
Georgi Donchev <booyaow24@gmail.com>
2024-11-12 21:25:10 +0000

    Added Exercise

---
+++ CSharp Fundamentals/09. Lists - Lab/05. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _05._Remove_Negatives_and_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList(); // 10 - 5 7 9 - 33 50
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    numbers.Remove(numbers[i]);
+                    i = 0 - 1;
+                }
+            }
+            if (numbers.Count < 1)
+            {
+                Console.WriteLine("empty");
+            }
+
+            else
+            {
+                numbers.Reverse();
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+        }
+    }
+}
