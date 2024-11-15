5ac4d4c41b37d26f518882a6ff538dabc2690d0e
Georgi Donchev <booyaow24@gmail.com>
2024-11-15 20:35:49 +0000

    Added Exercise

---
+++ CSharp Fundamentals/10. Lists - Exercise/07. Append Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _07._Append_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries).ToList();
+            List<string> newList = new List<string>();
+
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                List<string> element = list[i].Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                for (int x = 0; x < element.Count; x++)
+                {
+                    newList.Add(element[x]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", newList));
+        }
+    }
+}
