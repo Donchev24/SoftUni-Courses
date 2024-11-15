a056f43d451f309c6910e7d6e3abda15663ecf40
Georgi Donchev <booyaow24@gmail.com>
2024-11-15 20:34:31 +0000

    Added Exercise

---
+++ CSharp Fundamentals/10. Lists - Exercise/02. Change List/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _02._Change_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = Console.ReadLine().Split().ToList();
+
+            List<string> input = Console.ReadLine().Split().ToList();
+
+            while (input[0] != "end")
+            {
+                if (input[0] == "Delete")
+                {
+                    numbers.RemoveAll(item => item == input[1]);
+                }
+                else
+                {
+                    numbers.Insert(int.Parse(input[2]), input[1]);
+                }
+
+                input = Console.ReadLine().Split().ToList();
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
