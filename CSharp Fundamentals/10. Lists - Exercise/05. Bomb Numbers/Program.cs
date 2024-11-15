3dd5b4424e9f7f571cf629cbe52c8262b202a3c7
Georgi Donchev <booyaow24@gmail.com>
2024-11-15 20:35:21 +0000

    Added Exercise

---
+++ CSharp Fundamentals/10. Lists - Exercise/05. Bomb Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _05._Bomb_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> detonator = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int number = detonator[0];
+            int power = detonator[1];
+
+            while (list.Contains(number))
+            {
+                int index = list.IndexOf(number);
+
+                int leftIndex = Math.Max(0, index - power);
+                int rightIndex = Math.Min(list.Count - 1, index + power);
+
+                int range = rightIndex - leftIndex + 1;
+                list.RemoveRange(leftIndex, range);
+            }
+
+            Console.WriteLine(list.Sum());
+        }
+    }
+}
