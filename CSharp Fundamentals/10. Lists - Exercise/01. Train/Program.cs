0b8be66536015e50fa9f612d8d96e86944caa804
Georgi Donchev <booyaow24@gmail.com>
2024-11-15 20:33:43 +0000

    Added Exercise

---
+++ CSharp Fundamentals/10. Lists - Exercise/01. Train/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int maxCapacity = int.Parse(Console.ReadLine());
+
+            List<string> input = Console.ReadLine().Split().ToList();
+
+            while (input[0] != "end")
+            {
+                if (input.Count == 2)
+                {
+                    wagons.Add(int.Parse(input[1]));
+                }
+                else
+                {
+                    int passengers = int.Parse(input[0]);
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        if (wagons[i] + passengers <= maxCapacity)
+                        {
+                            wagons[i] += passengers;
+                            break;
+                        }
+                    }
+                }
+
+                input = Console.ReadLine().Split().ToList();
+            }
+
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
