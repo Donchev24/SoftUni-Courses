6cd6615a6ac696b1004fd326dcbeb94fc74d28ef
Georgi Donchev <booyaow24@gmail.com>
2024-11-17 16:36:40 +0000

    Added Exercise

---
+++ CSharp Fundamentals/11. Objects and Classes - Lab/04. Students/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _04._Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] studentDetails = input.Split();
+                Student student = new Student()
+                {
+                    FirstName = studentDetails[0],
+                    LastName = studentDetails[1],
+                    Age = studentDetails[2],
+                    HomeTown = studentDetails[3]
+                };
+
+                students.Add(student);
+            }
+
+            string city = Console.ReadLine();
+
+            foreach (Student student in students)
+            {
+                if (student.HomeTown == city)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+            }
+        }
+    }
+
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string HomeTown { get; set; }
+    }
+}
