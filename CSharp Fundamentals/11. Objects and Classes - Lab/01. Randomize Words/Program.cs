b436f969338de8d482450b6e3cbf318ddb318929
Georgi Donchev <booyaow24@gmail.com>
2024-11-17 16:35:28 +0000

    Added Exercise

---
+++ CSharp Fundamentals/11. Objects and Classes - Lab/01. Randomize Words/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _01._Randomize_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine().Split().ToList();
+
+            Random random = new Random();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                int randomIndex = random.Next(list.Count);
+
+                string currentWord = list[i];
+
+                list[i] = list[randomIndex];
+
+                list[randomIndex] = currentWord;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, list));
+        }
+    }
+}
