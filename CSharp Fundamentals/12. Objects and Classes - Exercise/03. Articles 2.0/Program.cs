e8dee7bc2ca84b4b0c218eb8fc7aae3ac0fe88b3
Georgi Donchev <booyaow24@gmail.com>
2024-11-21 21:12:22 +0000

    Added Exercise

---
+++ CSharp Fundamentals/12. Objects and Classes - Exercise/03. Articles 2.0/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _03._Articles_2._0
+{
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Article> articles = new List<Article>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] articleDetails = Console.ReadLine().Split(", ");
+
+                Article article = new Article(articleDetails[0], articleDetails[1], articleDetails[2]);
+
+                articles.Add(article);
+            }
+
+            foreach (Article article in articles)
+            {
+                Console.WriteLine(article);
+            }
+        }
+    }
+}
