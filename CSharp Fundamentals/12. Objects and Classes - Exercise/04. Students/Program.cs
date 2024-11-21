98fbf1b1f8400ce3edd9d6f271a7e5cc9d53c353
Georgi Donchev <booyaow24@gmail.com>
2024-11-21 21:12:33 +0000

    Added Exercise

---
+++ CSharp Fundamentals/12. Objects and Classes - Exercise/04. Students/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _04._Students
+{
+    class Student
+    {
+        public Student(string firstName, string lastName, double grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] studentDetails = Console.ReadLine().Split();
+
+                Student student = new Student(studentDetails[0], studentDetails[1], double.Parse(studentDetails[2]));
+
+                students.Add(student);
+            }
+
+            students = students.OrderByDescending(student => student.Grade).ToList();
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:F2}");
+            }
+        }
+    }
+}
