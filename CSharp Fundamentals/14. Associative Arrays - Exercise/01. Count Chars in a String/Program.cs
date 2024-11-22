848421b2d0a8aef655a45aed383126187ad7c21f
Georgi Donchev <booyaow24@gmail.com>
2024-11-22 20:38:51 +0000

    Added Exercise

---
+++ CSharp Fundamentals/14. Associative Arrays - Exercise/01. Count Chars in a String/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _01._Count_Chars_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char letter = input[i];
+
+                if (letter == ' ')
+                {
+                    continue;
+                }
+
+                if (!charCount.ContainsKey(letter))
+                {
+                    charCount[letter] = 0;
+                }
+
+                charCount[letter]++;
+            }
+
+            foreach (KeyValuePair<char, int> letter in charCount)
+            {
+                Console.WriteLine($"{letter.Key} -> {letter.Value}");
+            }
+        }
+    }
+}
