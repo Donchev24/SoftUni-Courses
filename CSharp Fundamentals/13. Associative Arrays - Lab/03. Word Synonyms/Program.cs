073acf21d64f429120463d2d0790cf3839d80ae3
Georgi Donchev <booyaow24@gmail.com>
2024-11-22 20:28:33 +0000

    Added Exercise

---
+++ CSharp Fundamentals/13. Associative Arrays - Lab/03. Word Synonyms/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _03._Word_Synonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> listOfSynonims = new List<string>();
+            Dictionary<string, List<string>> wordsAndSynonims = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string currentWord = Console.ReadLine();
+
+                string currentSynonim = Console.ReadLine();
+
+                if (!wordsAndSynonims.ContainsKey(currentWord))
+                {
+                    wordsAndSynonims[currentWord] = new List<string>();
+                }
+
+                wordsAndSynonims[currentWord].Add(currentSynonim);
+
+            }
+
+            foreach (KeyValuePair<string, List<string>> kvp in wordsAndSynonims)
+            {
+                Console.WriteLine($"{kvp.Key} - {string.Join(", ", kvp.Value)}");
+            }
+        }
+    }
+}
