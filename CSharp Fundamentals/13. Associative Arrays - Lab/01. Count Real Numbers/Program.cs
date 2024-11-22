289b5749cc2421dcb5ccbfa7b588a170786e99d0
Georgi Donchev <booyaow24@gmail.com>
2024-11-22 20:28:13 +0000

    Added Exercise

---
+++ CSharp Fundamentals/13. Associative Arrays - Lab/01. Count Real Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _01._Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = Console.ReadLine().Split().ToList();
+
+            SortedDictionary<string, int> result = new SortedDictionary<string, int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                string currentNum = numbers[i];
+
+
+                if (!result.ContainsKey(currentNum))
+                {
+                    result.Add(currentNum, 1);
+                }
+                else
+                {
+                    result[currentNum] += 1;
+                }
+
+            }
+
+            foreach (KeyValuePair<string, int> item in result)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
