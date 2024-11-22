d0d447ae66fa16a3c82416a712672c95a059a6ac
Georgi Donchev <booyaow24@gmail.com>
2024-11-22 20:39:10 +0000

    Added Exercise

---
+++ CSharp Fundamentals/14. Associative Arrays - Exercise/03. Orders/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _03._Orders
+{
+    class Product
+    {
+        public Product(string name, decimal price, decimal quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal Quantity { get; set; }
+    }
+     
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            Dictionary<string, Product> products = new Dictionary<string, Product>();
+
+            while ((input = Console.ReadLine()) != "buy")
+            {
+                string[] productArgs = input.Split();
+                string name = productArgs[0];
+                decimal price = decimal.Parse(productArgs[1]);
+                decimal quantity = decimal.Parse(productArgs[2]);
+
+                Product product = new Product(name, price, quantity);
+
+                if (!products.ContainsKey(name))
+                {
+                    products.Add(name, product);
+                }
+                else
+                {
+                    products[name].Price = price;
+                    products[name].Quantity += quantity;
+                }
+            }
+            decimal totalPrice = 0;
+
+            foreach (var P in products)
+            {
+                Console.WriteLine($"{P.Key} -> {P.Value.Price * P.Value.Quantity:F2}");
+            }
+        }
+    }
+}
