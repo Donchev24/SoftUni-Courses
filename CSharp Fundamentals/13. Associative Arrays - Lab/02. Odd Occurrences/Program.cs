17594f0921f9eeddce8f7f500e37b51862781c17
Georgi Donchev <booyaow24@gmail.com>
2024-11-22 20:28:23 +0000

    Added Exercise

---
+++ CSharp Fundamentals/13. Associative Arrays - Lab/02. Odd Occurrences/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _02._Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine()
+                .Split()
+                .Select(x => x.ToLower())
+                .ToList();
+
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                string currentWord = words[i];
+
+                if (!result.ContainsKey(currentWord))
+                {
+                    result.Add(currentWord, 0);
+                }
+                result[currentWord]++;
+            }
+
+            foreach (KeyValuePair<string, int> kvp in result)
+            {
+                if (kvp.Value % 2 != 0)
+                {
+                    Console.Write($"{kvp.Key} ");
+                }
+            }
+        }
+    }
+}
