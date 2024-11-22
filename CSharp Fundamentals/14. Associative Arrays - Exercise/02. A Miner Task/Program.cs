8bee939428385de3c1cff6cde6c0b3cf9f43a2c5
Georgi Donchev <booyaow24@gmail.com>
2024-11-22 20:39:00 +0000

    Added Exercise

---
+++ CSharp Fundamentals/14. Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string resource = string.Empty;
+            Dictionary<string, double> resourceCount = new Dictionary<string, double>();
+
+            while ((resource = Console.ReadLine()) != "stop")
+            {
+                double quantity = double.Parse(Console.ReadLine());
+
+                if (!resourceCount.ContainsKey(resource))
+                {
+                    resourceCount.Add(resource, 0);
+                }
+
+                resourceCount[resource] += quantity;
+            }
+
+            foreach (var r in resourceCount)
+            {
+                Console.WriteLine($"{r.Key} -> {r.Value}");
+            }
+        }
+    }
+}
