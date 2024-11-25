4929ab979c81c4afabb163af30e49fcd9beb14ab
Georgi Donchev <booyaow24@gmail.com>
2024-11-25 18:18:16 +0000

    Added Exercise

---
+++ CSharp Fundamentals/15. Text Processing - Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _01._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = string.Empty;
+
+            while ((word = Console.ReadLine()) != "end")
+            {
+                string reversedWord = new string(word.Reverse().ToArray());
+                Console.WriteLine($"{word} = {reversedWord}");
+            }
+        }
+    }
+}
