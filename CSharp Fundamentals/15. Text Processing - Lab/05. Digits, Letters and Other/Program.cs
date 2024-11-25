a56b33b4bb7194f34abbc9ce3954dc4f6b7ec298
Georgi Donchev <booyaow24@gmail.com>
2024-11-25 18:18:59 +0000

    Added Exercise

---
+++ CSharp Fundamentals/15. Text Processing - Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _05._Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string digits = new String(input.Where(Char.IsDigit).ToArray());
+            char[] removedDigits = digits.ToCharArray();
+
+            for (int i = 0; i < removedDigits.Length; i++)
+            {
+                int start = input.IndexOf(removedDigits[i]);
+                input = input.Remove(start, 1);
+            }
+
+            string letters = new String(input.Where(Char.IsLetter).ToArray());
+            char[] removedLetters = letters.ToCharArray();
+
+            for (int i = 0; i < removedLetters.Length; i++)
+            {
+                int start = input.IndexOf(removedLetters[i]);
+                input = input.Remove(start, 1);
+            }
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(input);
+        }
+    }
+}
