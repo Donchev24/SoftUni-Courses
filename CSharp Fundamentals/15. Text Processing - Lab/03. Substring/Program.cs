119dbdcb8330a30796a1f2b82d0d550cf29dd638
Georgi Donchev <booyaow24@gmail.com>
2024-11-25 18:18:38 +0000

    Added Exercise

---
+++ CSharp Fundamentals/15. Text Processing - Lab/03. Substring/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _03._Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string textToCut = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            while (text.Contains(textToCut))
+            {
+                int start = text.IndexOf(textToCut);
+                int end = textToCut.Length;
+                text = text.Remove(start, end);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
