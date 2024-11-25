ea0cbf0b4a33cdac3abf4ad858b58a598016bed1
Georgi Donchev <booyaow24@gmail.com>
2024-11-25 18:38:07 +0000

    Added Exercise

---
+++ CSharp Fundamentals/16. Text Processing - Exercise/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _06._Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
