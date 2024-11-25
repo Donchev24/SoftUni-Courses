114956f1669461c162ab110ed6eb47a7cea1010c
Georgi Donchev <booyaow24@gmail.com>
2024-11-25 18:18:28 +0000

    Added Exercise

---
+++ CSharp Fundamentals/15. Text Processing - Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace _02._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string currentWord = input[i];
+                int length = currentWord.Length;
+
+                for (int y = 0; y < length; y++)
+                {
+                    sb.Append(currentWord);
+                }
+            }
+
+            Console.Write(sb);
+        }
+    }
+}
