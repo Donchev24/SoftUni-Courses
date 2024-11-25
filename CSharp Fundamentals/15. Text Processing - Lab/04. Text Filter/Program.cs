3d7cada999081b16ad7052e49e8d68b5529fbcd4
Georgi Donchev <booyaow24@gmail.com>
2024-11-25 18:18:48 +0000

    Added Exercise

---
+++ CSharp Fundamentals/15. Text Processing - Lab/04. Text Filter/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _04._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ");
+
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < bannedWords.Length; i++)
+            {
+                string currentWord = bannedWords[i];
+                int length = currentWord.Length;
+                text = text.Replace(currentWord, new string('*', length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
