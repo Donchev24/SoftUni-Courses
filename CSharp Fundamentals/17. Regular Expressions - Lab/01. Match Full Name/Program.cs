12e3c6652ec0dbf6b9ecf155dd56dfb39b7d5488
Georgi Donchev <booyaow24@gmail.com>
2024-11-27 03:42:35 +0000

    Added Exercise

---
+++ CSharp Fundamentals/17. Regular Expressions - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]{1,} [A-Z][a-z]{1,}\b";
+            string input = Console.ReadLine();
+
+            RegexOptions options = RegexOptions.Multiline;
+
+            foreach (Match m in Regex.Matches(input, pattern, options))
+            {
+                Console.Write($"{m.Value} ");
+            }
+        }
+    }
+}
