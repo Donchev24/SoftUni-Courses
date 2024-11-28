1091428c3649cc07f720cee3efb0052d61503fb3
Georgi Donchev <booyaow24@gmail.com>
2024-11-28 20:38:05 +0000

    Added Exercise

---
+++ CSharp Advanced/01. Stacks and Queues - Lab/03. Simple Calculator/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _03._Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] reversedInput = input.Reverse().ToArray();
+
+            Stack<string> stack = new Stack<string>(reversedInput);
+
+            int sum = int.Parse(stack.Pop());
+
+            char currentOperation = '+';
+
+            while (stack.Any())
+            {
+                if (stack.Peek() == "+" || stack.Peek() == "-")
+                {
+                    currentOperation = char.Parse(stack.Pop());
+                }
+                else if (stack.Peek() != "+" && stack.Peek() != "-")
+                {
+                    if (currentOperation == '+')
+                    {
+                        sum += int.Parse(stack.Pop());
+                    }
+                    else 
+                    {
+                        sum -= int.Parse(stack.Pop());
+                    }
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
