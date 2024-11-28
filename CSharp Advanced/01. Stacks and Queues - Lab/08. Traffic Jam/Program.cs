7a9224b290fe605c4ff1193e3557b83a69e3b122
Georgi Donchev <booyaow24@gmail.com>
2024-11-28 20:39:03 +0000

    Added Exercise

---
+++ CSharp Advanced/01. Stacks and Queues - Lab/08. Traffic Jam/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _08._Traffic_Jam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int allowedPasses = int.Parse(Console.ReadLine());
+
+            Queue<string> trafficJam = new Queue<string>();
+
+            int passedCars = 0;
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                if (command == "green")
+                {
+                    for (int i = 1; i <= allowedPasses; i++)
+                    {
+                        if (trafficJam.Any())
+                        {
+                            Console.WriteLine($"{trafficJam.Dequeue()} passed!");
+                            passedCars++;
+                        }
+                    }
+                }
+                else
+                {
+                    trafficJam.Enqueue(command);
+                }
+
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{passedCars} cars passed the crossroads.");
+        }
+    }
+}
