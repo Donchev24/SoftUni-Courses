7f1036e1f72cd1a985332cba4f8de0618fae9b35
Georgi Donchev <booyaow24@gmail.com>
2024-11-28 20:38:45 +0000

    Added Exercise

---
+++ CSharp Advanced/01. Stacks and Queues - Lab/6. Supermarket/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _6._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> list = new Queue<string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    while (list.Any())
+                    {
+                        Console.WriteLine(list.Dequeue());
+                    }
+                }
+                else
+                {
+                    list.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{list.Count} people remaining.");
+        }
+    }
+}
