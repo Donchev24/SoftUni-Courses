b68ba7ac8a514fc903ed8d943808dbcbabc22f05
Georgi Donchev <booyaow24@gmail.com>
2024-11-28 20:37:45 +0000

    Added Exercise

---
+++ CSharp Advanced/01. Stacks and Queues - Lab/01. Reverse a String/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _01._Reverse_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            
+            Stack<char> reversedStr = new Stack<char>(input);
+
+           
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.Write(reversedStr.Pop());
+            }
+        }
+    }
+}
