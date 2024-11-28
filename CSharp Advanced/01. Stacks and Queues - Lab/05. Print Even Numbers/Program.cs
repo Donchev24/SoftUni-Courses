bf8090a6b1e58b43749c1f4a36e4f438d7adca53
Georgi Donchev <booyaow24@gmail.com>
2024-11-28 20:38:26 +0000

    Added Exercise

---
+++ CSharp Advanced/01. Stacks and Queues - Lab/05. Print Even Numbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace _05._Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>(numbers);
+
+            Queue<int> evenNumbers = new Queue<int>();
+
+            while (queue.Any())
+            {
+                int currentNumber = queue.Dequeue();
+
+                if (currentNumber % 2 == 0)
+                {
+                    evenNumbers.Enqueue(currentNumber);
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            while (evenNumbers.Any())
+            {
+                sb.Append(evenNumbers.Dequeue());
+                if (evenNumbers.Any())
+                {
+                    sb.Append(", ");
+                }
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
