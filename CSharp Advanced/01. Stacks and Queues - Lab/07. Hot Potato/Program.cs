eb17a59e5dbbea789b272de212a1286fe20f622b
Georgi Donchev <booyaow24@gmail.com>
2024-11-28 20:38:54 +0000

    Added Exercise

---
+++ CSharp Advanced/01. Stacks and Queues - Lab/07. Hot Potato/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _07._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Queue<string> kids = new Queue<string>(input);
+
+            int hotPotato = int.Parse(Console.ReadLine());
+
+            while (kids.Count > 1)
+            {
+                for (int i = 1; i <= hotPotato; i++)
+                {
+                    if (i == hotPotato)
+                    {
+                        Console.WriteLine($"Removed {kids.Dequeue()}");
+                    }
+                    else
+                    {
+                        kids.Enqueue(kids.Dequeue());
+                    }
+                }
+            }
+
+            Console.WriteLine($"Last is {kids.Dequeue()}");
+        }
+    }
+}
