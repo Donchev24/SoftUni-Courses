95223aee8d0b740428aa2ffaee0eb6963e84c8b1
Georgi Donchev <booyaow24@gmail.com>
2024-11-28 20:38:17 +0000

    Added Exercise

---
+++ CSharp Advanced/01. Stacks and Queues - Lab/04. Matching Brackets/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _04._Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                
+                if (input[i] == '(')
+                {
+                    stack.Push(i);
+                }
+
+                else if (input[i] == ')')
+                {
+                    if (stack.Any())
+                    {
+                        int startIndex = stack.Pop();
+                        string match = input.Substring(startIndex, i - startIndex + 1);
+                        Console.WriteLine(match);
+                    }
+                }
+
+            }
+        }
+    }
+}
