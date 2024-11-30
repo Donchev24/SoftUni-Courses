66f8cac97400135fb3894e5d8b37332aa03dea3a
Georgi Donchev <booyaow24@gmail.com>
2024-11-30 12:06:06 +0000

    Added Exercise

---
+++ CSharp Advanced/02. Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _05._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> values = new Stack<int>(input);
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+
+            int currentRackCap = 0;
+
+            int racks = 1;
+
+            while (values.Any())
+            {
+                int currentValue = values.Pop();
+
+                if (currentValue + currentRackCap <= rackCapacity)
+                {
+                    currentRackCap += currentValue;
+                }
+                else
+                {
+                    racks++;
+                    currentRackCap = currentValue;
+                }
+            }
+
+            Console.WriteLine(racks);
+
+        }
+    }
+}
