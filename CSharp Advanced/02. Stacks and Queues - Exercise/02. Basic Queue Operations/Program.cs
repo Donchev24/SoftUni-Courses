2fbdf73618d1d920b97f746f0d17c45f896b384c
Georgi Donchev <booyaow24@gmail.com>
2024-11-30 12:05:37 +0000

    Added Exercise

---
+++ CSharp Advanced/02. Stacks and Queues - Exercise/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _02._Basic_Queue_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int toPush = tokens[0];
+            int toPop = tokens[1];
+            int toFind = tokens[2];
+
+            int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>();
+
+            if (input.Length >= toPush)
+            {
+                for (int i = 0; i < toPush; i++)
+                {
+                    queue.Enqueue(input[i]);
+                }
+            }
+
+            if (toPop <= queue.Count)
+            {
+                for (int i = 0; i < toPop; i++)
+                {
+                    queue.Dequeue();
+                }
+            }
+
+            if (queue.Contains(toFind))
+            {
+                Console.WriteLine("true");
+            }
+            else if (!queue.Any())
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                Console.WriteLine($"{queue.Min()}");
+            }
+        }
+    }
+}
