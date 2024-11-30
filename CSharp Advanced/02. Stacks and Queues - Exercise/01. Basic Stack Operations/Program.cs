255567a34ae3a3400b9cbde68b20ed1d5ed5628e
Georgi Donchev <booyaow24@gmail.com>
2024-11-30 12:05:27 +0000

    Added Exercise

---
+++ CSharp Advanced/02. Stacks and Queues - Exercise/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _01._Basic_Stack_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int toPush = tokens[0];
+            int toPop = tokens[1];
+            int toFind = tokens[2];
+
+            int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Stack<int> stack = new Stack<int>();
+
+            if (input.Length >= toPush)
+            {
+                for (int i = 0; i < toPush; i++)
+                {
+                    stack.Push(input[i]);
+                }
+            }
+
+            if (toPop <= stack.Count)
+            {
+                for(int i = 0;i < toPop; i++)
+                {
+                    stack.Pop();
+                }
+            }
+
+            if (stack.Contains(toFind))
+            {
+                Console.WriteLine("true");
+            }
+            else if (!stack.Any())
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                Console.WriteLine($"{stack.Min()}");
+            }
+        }
+    }
+}
