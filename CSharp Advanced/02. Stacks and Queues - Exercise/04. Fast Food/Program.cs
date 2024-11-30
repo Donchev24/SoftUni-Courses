26f1d2f24936a335db6de018121c8ccc3eb9f68c
Georgi Donchev <booyaow24@gmail.com>
2024-11-30 12:05:56 +0000

    Added Exercise

---
+++ CSharp Advanced/02. Stacks and Queues - Exercise/04. Fast Food/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _04._Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> orders = new Queue<int>(input);
+
+            Console.WriteLine(orders.Max());
+
+            while (orders.Any() && foodQuantity >= orders.Peek())
+            {
+                foodQuantity -= orders.Dequeue();
+
+            }
+
+            if (orders.Any())
+            {
+                Console.Write($"Orders left: {string.Join(" ", orders)}");
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
