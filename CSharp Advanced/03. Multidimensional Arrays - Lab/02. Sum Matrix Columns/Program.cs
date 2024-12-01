dc5226bf9443ffeb98877f268a36f62f81d89752
Georgi Donchev <booyaow24@gmail.com>
2024-12-01 20:35:59 +0000

    Added Exercise

---
+++ CSharp Advanced/03. Multidimensional Arrays - Lab/02. Sum Matrix Columns/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _02._Sum_Matrix_Columns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimentions = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = dimentions[0];
+            int cols = dimentions[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            int sum = 0;
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    sum += matrix[row, col];
+                }
+
+                Console.WriteLine(sum);
+                sum = 0;
+            }
+        }
+    }
+}
