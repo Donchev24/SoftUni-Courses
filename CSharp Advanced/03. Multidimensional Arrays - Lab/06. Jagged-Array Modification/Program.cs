adf94c30a2887d4171375f52f6a7bd83226c689e
Georgi Donchev <booyaow24@gmail.com>
2024-12-01 20:36:36 +0000

    Added Exercise

---
+++ CSharp Advanced/03. Multidimensional Arrays - Lab/06. Jagged-Array Modification/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _06._Jagged_Array_Modification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            int[][] jaggedArr = new int[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                jaggedArr[i] = input;
+            }
+
+            string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (command[0] != "END")
+            {
+                int row = int.Parse(command[1]);
+                int col = int.Parse(command[2]);
+                int value = int.Parse(command[3]);
+
+                if (row < 0 || row >=jaggedArr.Length || col < 0 || col >= jaggedArr[row].Length)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                    command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    continue;
+                }
+                if (command[0] == "Add")
+                {
+                    jaggedArr [row][col] += value;
+                }
+                else if (command[0] == "Subtract")
+                {
+                    jaggedArr[row][col] -= value;
+                }
+
+                command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            for (int row = 0; row < jaggedArr.Length; row++)
+            {
+                Console.WriteLine($"{string.Join(" ", jaggedArr[row])}");
+            }
+        }
+    }
+}
