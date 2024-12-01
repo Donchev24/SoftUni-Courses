8dd0e5585bbe19ed9194a1b6be2e062eac2b37ac
Georgi Donchev <booyaow24@gmail.com>
2024-12-01 20:35:30 +0000

    Added Exercise

---
+++ CSharp Advanced/03. Multidimensional Arrays - Lab/01. Sum Matrix Elements/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _01._Sum_Matrix_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimentions = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = dimentions[0];
+            int cols = dimentions[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            int sum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    sum += matrix[row, col];
+                }
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
+
+        }
+    }
+}
