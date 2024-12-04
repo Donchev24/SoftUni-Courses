64824c3e79c303cbd44a82c2b610f335d40f81a9
Georgi Donchev <booyaow24@gmail.com>
2024-12-04 20:25:20 +0000

    Added Exercise

---
+++ CSharp Advanced/04. Multidimensional Arrays - Exercise/03. Maximal Sum/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace _03._Maximal_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimentions = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = dimentions[0];
+            int cols = dimentions[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] data = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                   matrix[row, col] = data[col];
+                }
+            }
+
+            int maxSum = int.MinValue;
+            int maxSumRow = -1;
+            int maxSumCol = -1;
+
+            for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 2; col++)
+                {
+                    int currentSum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2]
+                                   + matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2]
+                                   + matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+
+                    if (currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+                        maxSumRow = row;
+                        maxSumCol = col;
+                    }
+                }
+            }
+
+            int[,] maxSumMatrix = new int[3, 3];
+
+            Console.WriteLine($"Sum = {maxSum}");
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    maxSumMatrix[row, col] = matrix[maxSumRow + row, maxSumCol + col];
+                    Console.Write($"{maxSumMatrix[row, col]} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
