4f64e2cde4817b26950a94d808bfd06b2255c04b
Georgi Donchev <booyaow24@gmail.com>
2024-12-09 20:27:30 +0000

    Added Exercise

---
+++ CSharp Advanced/05. Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _07._Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> parkedCars = new();
+
+            string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (input[0] != "END")
+            {
+                string direction = input[0];
+                string registration = input[1];
+
+                if (direction == "IN")
+                {
+                    parkedCars.Add(registration);
+                }
+                else
+                {
+                    parkedCars.Remove(registration);
+                }
+
+                input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            if (parkedCars.Any())
+            {
+                foreach (string car in parkedCars)
+                {
+                    Console.WriteLine(car); 
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
