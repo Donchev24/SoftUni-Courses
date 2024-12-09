bf6d2093a03d141c3dfa30d2ce50d3e6557a3b53
Georgi Donchev <booyaow24@gmail.com>
2024-12-09 20:27:18 +0000

    Added Exercise

---
+++ CSharp Advanced/05. Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _06._Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> nameList = new();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+
+                nameList.Add(name);
+            }
+
+            foreach (string name in nameList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
