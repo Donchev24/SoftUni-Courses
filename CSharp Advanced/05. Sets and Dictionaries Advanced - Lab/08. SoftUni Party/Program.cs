7bf708a56b63fdd66be1422692458f21bcf32af9
Georgi Donchev <booyaow24@gmail.com>
2024-12-09 20:27:44 +0000

    Added Exercise

---
+++ CSharp Advanced/05. Sets and Dictionaries Advanced - Lab/08. SoftUni Party/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace _08._SoftUni_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> vip = new();
+            HashSet<string> regular = new();
+
+            string input = Console.ReadLine();
+
+            while (input != "PARTY")
+            {
+                if (input[0] == '0' || input[0] == '1' || input[0] == '2' || input[0] == '3' ||
+                    input[0] == '4' || input[0] == '5' || input[0] == '6' || input[0] == '7' ||
+                    input[0] == '8' || input[0] == '9')
+                {
+                    vip.Add(input);
+                }
+                else
+                {
+                    regular.Add(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            while (input != "END")
+            {
+                if (vip.Contains(input))
+                {
+                    vip.Remove(input);
+                }
+                if (regular.Contains(input))
+                {
+                    regular.Remove(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(vip.Count + regular.Count);
+
+            if (vip.Any())
+             {
+                 foreach (string g in vip)
+                 {
+                     Console.WriteLine(g);
+                 }
+             }
+
+             if (regular.Any())
+             {
+                 foreach (string g in regular)
+                 {
+                     Console.WriteLine(g);
+                 }
+             }
+        }
+    }
+}
