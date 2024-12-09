71d5e399c15d5c9dbb18271b1ed14126c236e20b
Georgi Donchev <booyaow24@gmail.com>
2024-12-09 20:26:15 +0000

    Added Exercise

---
+++ CSharp Advanced/05. Sets and Dictionaries Advanced - Lab/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _03._Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            numbers = numbers.OrderByDescending(n  => n).Take(3).ToArray();
+
+            Console.Write(string.Join(" ", numbers));            
+        }
+    }
+}
