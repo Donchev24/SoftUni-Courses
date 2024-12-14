57e33b54e4f2ec2be781938635c703af87ef0dcd
Georgi Donchev <booyaow24@gmail.com>
2024-12-14 17:56:22 +0000

    Added Exercise

---
+++ CSharp Advanced/06. Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _02._Sets_of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nmLengths = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = nmLengths[0];
+            int m = nmLengths[1];
+
+            HashSet<int> nSet = new();
+            HashSet<int> mSet = new();
+
+            for (int i = 1; i <= n + m; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+
+                if (i <= n)
+                {
+                    nSet.Add(currentNum);
+                }
+                else
+                {
+                    mSet.Add(currentNum);
+                }
+            }
+
+            foreach (int i in nSet)
+            {
+                if (mSet.Contains(i))
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
