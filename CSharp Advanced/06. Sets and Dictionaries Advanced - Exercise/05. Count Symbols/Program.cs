f61dda4d3a5be9802aab830bdaf76b697f1118f3
Georgi Donchev <booyaow24@gmail.com>
2024-12-14 17:56:51 +0000

    Added Exercise

---
+++ CSharp Advanced/06. Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            char[] text = input.ToCharArray();
+
+            Dictionary<char, int> symbolCount = new();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char symbol = text[i];
+
+                if (!symbolCount.ContainsKey(symbol))
+                {
+                    symbolCount.Add(symbol, 0);
+                }
+
+                symbolCount[symbol]++;
+            }
+
+            foreach (var (symbol, count) in symbolCount.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{symbol}: {count} time/s");
+            }
+        }
+    }
+}
