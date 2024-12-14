1b27add9b11df49fea541ca33066a48c71183aac
Georgi Donchev <booyaow24@gmail.com>
2024-12-14 17:56:11 +0000

    Added Exercise

---
+++ CSharp Advanced/06. Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _01._Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> usernames = new();
+
+            for (int i = 0; i < n; i++)
+            {
+                string username = Console.ReadLine();
+
+                if (!usernames.Contains(username))
+                {
+                    usernames.Add(username);
+                    Console.WriteLine(username);
+                }
+            }
+        }
+    }
+}
