5eed74b12c32932ae86ed157444bbeca91ef80fd
Georgi Donchev <booyaow24@gmail.com>
2024-12-14 17:56:41 +0000

    Added Exercise

---
+++ CSharp Advanced/06. Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _04._Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> numCounts = new();
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+
+                if (!numCounts.ContainsKey(currentNum))
+                {
+                    numCounts.Add(currentNum, 0);
+                }
+
+                numCounts[currentNum]++;
+            }
+
+            foreach (var (k, v) in numCounts)
+            {
+                if (v % 2 == 0)
+                {
+                    Console.WriteLine(k);
+                }
+            }
+        }
+    }
+}
