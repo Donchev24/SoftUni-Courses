5fc02075cfeac08769e5441ea892cfa90af9b5bc
Georgi Donchev <booyaow24@gmail.com>
2024-12-14 17:56:31 +0000

    Added Exercise

---
+++ CSharp Advanced/06. Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> elements = new();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] currentElement = Console.ReadLine().Split();
+
+                foreach (string element in currentElement)
+                {
+                    elements.Add(element);
+                }
+            }
+
+            Console.WriteLine($"{string.Join(" ", elements.OrderBy(e => e))}");
+        }
+    }
+}
