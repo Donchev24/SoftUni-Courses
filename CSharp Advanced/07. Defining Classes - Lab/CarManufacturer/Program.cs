bfa8362daa3baa2f755ff135f7d98673f67f0840
Georgi Donchev <booyaow24@gmail.com>
2024-12-15 16:50:06 +0000

    Added Exercise

---
+++ CSharp Advanced/07. Defining Classes - Lab/CarManufacturer/Program.cs
@@ -0,0 +1,116 @@
+﻿using System.Reflection;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Tire[]> tiresList = new List<Tire[]>();
+
+            string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (command[0] != "No")
+            {
+                Tire[] currentTiresInfo = new Tire[4];
+
+                int tireYear = 0;
+                double tirePressure = 0;
+
+                int counter = -1;
+
+                for (int i = 0; i < command.Length; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        tireYear = int.Parse(command[i]);
+                    }
+                    else
+                    {
+                        counter++;
+
+                        tirePressure = double.Parse(command[i]);
+
+                        Tire tire = new Tire(tireYear, tirePressure);
+                        currentTiresInfo[counter] = tire;
+                    }
+                }
+
+                tiresList.Add(currentTiresInfo);
+
+                command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+    
+
+            List<Engine> engineList = new List<Engine>();
+
+            string[] command2 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (command2[0] != "Engines")
+            {
+                int horsePower = 0;
+                double cubicCapacity = 0;
+
+                for (int i = 0; i < command2.Length; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        horsePower = int.Parse(command2[i]);
+                    }
+                    else
+                    {
+                        cubicCapacity = double.Parse(command2[i]);
+                    }
+                }
+
+                Engine engine = new Engine(horsePower, cubicCapacity);
+                engineList.Add(engine);
+
+                command2 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+
+            List<Car> carList = new List<Car>();
+
+            string[] command3 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+
+            while (command3[0] != "Show")
+            {
+                string make = command3[0];
+                string model = command3[1];
+                int year = int.Parse(command3[2]);
+                double fuelQuantity = double.Parse(command3[3]);
+                double fuelConsumption = double.Parse(command3[4]);
+                Engine engine = engineList[int.Parse(command3[5])];
+                Tire[] tires = tiresList[int.Parse(command3[6])];
+
+                Car car = new Car(make,model,year,fuelQuantity, fuelConsumption, engine, tires);
+
+                carList.Add(car);
+
+                //{ make}
+                //{ model}
+                //{ year}
+                //{ fuelQuantity}
+                //{ fuelConsumption}
+                //{ engineIndex}
+                //{ tiresIndex}
+
+                command3 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+
+            IEnumerable<Car> filteredCars = carList.Where(car => car.Year >= 2017 &&
+                                                      car.Engine.HorsePower > 330 &&
+                                                      car.Tires.Sum(t => t.Pressure) > 9 && car.Tires.Sum(t => t.Pressure) < 10);
+
+            foreach (Car car in filteredCars)
+            {
+                car.Drive();
+                Console.WriteLine(car.WhoAmI());
+            }
+        }
+    }
+}
---
+++ CSharp Advanced/07. Defining Classes - Lab/CarManufacturer/Tire.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+   public class Tire
+    {
+        private int year;
+        private double pressure;
+        private double pressureSum;
+
+        
+        public Tire(int year, double pressure)
+        {
+            this.Year = year;
+            this.Pressure = pressure;
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        public double Pressure
+        {
+            get { return pressure; }
+            set { pressure = value; }
+        }
+        public double PressureSum
+        {
+            get { return pressureSum; }
+            set { pressureSum = value; }
+        }
+    }
+}
