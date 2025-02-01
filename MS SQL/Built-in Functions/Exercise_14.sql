5538e829702f61783f984165e95cefc1804222e4
Georgi Donchev <booyaow24@gmail.com>
2025-02-01 15:44:59 +0000

    Added Exercise

---
+++ MS SQL/Built-in Functions/Exercise_14.sql
@@ -0,0 +1,11 @@
+  SELECT
+     TOP (50)
+	     [Name],
+		 Format([Start], 'yyyy-MM-dd')
+	  AS [Start]
+    FROM [Games]
+   WHERE DATEPART(YEAR, [Start]) IN (2011, 2012)
+ORDER BY [Start],
+         [Name]
+  
+
