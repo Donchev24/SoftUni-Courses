489370108e4855bcca0b10955fadf80901a06d0f
Georgi Donchev <booyaow24@gmail.com>
2025-02-03 21:31:23 +0000

    Added Exercise

---
+++ MS SQL/Subqueries and Joins/Exercise_03.sql
@@ -0,0 +1,11 @@
+  SELECT EmployeeID,
+         FirstName,
+	     LastName,
+	     d.[Name]
+    FROM Employees
+      AS e
+    JOIN Departments
+      AS d
+	  ON e.DepartmentID = d.DepartmentID
+   WHERE d.[Name] = 'Sales'
+ORDER BY EmployeeID
