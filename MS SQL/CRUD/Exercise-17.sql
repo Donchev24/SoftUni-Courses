eaa533775935022117764569a5023155d35b5af7
Georgi Donchev <booyaow24@gmail.com>
2025-02-18 18:20:30 +0000

    Added Exercise

---
+++ MS SQL/CRUD/Exercise-17.sql
@@ -0,0 +1,14 @@
+CREATE
+  VIEW [V_EmployeeNameJobTitle]
+    AS (
+	         SELECT 
+		     CONCAT ([FirstName],
+			        ' ',
+					[MiddleName],
+					' ',
+					[LastName]
+					)
+				 AS [Full Name],
+				    [JobTitle]
+			   FROM [Employees]
+	   )
