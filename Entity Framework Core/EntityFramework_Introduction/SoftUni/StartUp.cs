f6e9ca673cdf620c35cd1e20bd9482b01edcf6ab
Georgi Donchev <booyaow24@gmail.com>
2025-02-26 14:31:40 +0000

    Added Exercise Method

---
+++ Entity Framework Core/EntityFramework_Introduction/SoftUni/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext dbContext = new SoftUniContext();
+            dbContext.Database.EnsureCreated();
+
+            string result = GetEmployeesFullInformation(dbContext);
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary
+                })
+                .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary.ToString("F2")}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
