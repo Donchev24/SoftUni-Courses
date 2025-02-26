d2283497330f248dda1437fe231b56ddb20c8eb9
Georgi Donchev <booyaow24@gmail.com>
2025-02-26 13:21:13 +0000

    Added DB-First Type of Project

---
+++ Entity Framework Core/EntityFramework_Introduction/SoftUni/Models/Town.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace SoftUni.Models
+{
+    using System;
+    using System.Collections.Generic;
+    public class Town
+    {
+        public Town()
+        {
+            Addresses = new HashSet<Address>();
+        }
+
+        public int TownId { get; set; }
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<Address> Addresses { get; set; }
+    }
+}
