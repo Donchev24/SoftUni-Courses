a02638fcb100156f0439e94ab606d6c97447b9aa
Georgi Donchev <booyaow24@gmail.com>
2025-03-10 18:44:32 +0000

    Added DbContext

---
+++ Entity Framework Core/Entity_Relations/StudentSystem/P01_StudentSystem.Data.Common/ApplicationCommonConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace P01_StudentSystem.Data.Common
+{
+    public static class ApplicationCommonConfiguration
+    {
+        public const string ConnectionString
+            = @"Server=.;Database=StudentSystem;Trusted_Connection=True;";
+    }
+}
---
+++ Entity Framework Core/Entity_Relations/StudentSystem/P01_StudentSystem.Data/StudentSystemContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+using static P01_StudentSystem.Data.Common.ApplicationCommonConfiguration;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        protected StudentSystemContext()
+        {
+        }
+        public StudentSystemContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Course> Course { get; set; }
+        public virtual DbSet<Homework> Homeworks { get; set; }
+        public virtual DbSet<Resource> Resources { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<StudentCourse> StudentsCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder
+                .Entity<StudentCourse>()
+                .HasKey(sc => new { sc.StudentId, sc.CourseId });
+        }
+
+        
+
+
+    }
+}
