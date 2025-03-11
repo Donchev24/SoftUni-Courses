bcf6f779d5a21678cc37a4a3635457ef3f6b04bd
Georgi Donchev <booyaow24@gmail.com>
2025-03-11 21:32:25 +0000

    Configured EntityValidationConstants

---
+++ Entity Framework Core/LINQ/MusicHub/Data/Models/EntityValidationConstants.cs
@@ -0,0 +1,31 @@
+﻿namespace MusicHub.Data.Models
+{
+    public static class EntityValidationConstants
+    {
+        public static class SongConstants
+        {
+            public const int SongNameMaxLength = 20;
+        }
+
+        public static class AlbumConstants
+        {
+            public const int AlbumNameMaxLength = 40;
+        }
+
+        public static class PerformerConstants
+        {
+            public const int FirstNameMaxLength = 20;
+            public const int LastNameMaxLength = 20;
+        }
+
+        public static class ProducerConstants
+        {
+            public const int ProducerNameMaxLength = 30;
+        }
+
+        public static class WriterConstants
+        {
+            public const int WriterNameMaxLength = 20;
+        }
+    }
+}
