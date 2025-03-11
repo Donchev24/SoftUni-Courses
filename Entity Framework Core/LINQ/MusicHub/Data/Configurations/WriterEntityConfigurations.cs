da5f14afaee89797fccc8f1b7d6f82594c4b09d9
Georgi Donchev <booyaow24@gmail.com>
2025-03-11 21:31:57 +0000

    Added WriterEntityConfigurations

---
+++ Entity Framework Core/LINQ/MusicHub/Data/Configurations/WriterEntityConfigurations.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MusicHub.Data.Models;
+using static MusicHub.Data.Models.EntityValidationConstants.WriterConstants;
+
+namespace MusicHub.Data.Configurations
+{
+    public class WriterEntityConfigurations : IEntityTypeConfiguration<Writer>
+    {
+        public void Configure(EntityTypeBuilder<Writer> entity)
+        {
+            entity
+                .HasKey(w => w.Id);
+
+            entity
+                .Property(w => w.Name)
+                .IsRequired()
+                .IsUnicode()
+                .HasMaxLength(WriterNameMaxLength);
+
+            entity
+                .Property(w => w.Pseudonym)
+                .IsRequired(false);
+        }
+    }
+}
