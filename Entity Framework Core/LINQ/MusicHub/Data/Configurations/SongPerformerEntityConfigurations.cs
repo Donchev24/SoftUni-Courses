95328fb64acd66f1afd03447bc53798c14f4e3c5
Georgi Donchev <booyaow24@gmail.com>
2025-03-11 21:31:34 +0000

    Added SongPerformerEntityConfigurations

---
+++ Entity Framework Core/LINQ/MusicHub/Data/Configurations/SongPerformerEntityConfigurations.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MusicHub.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicHub.Data.Configurations
+{
+    public class SongPerformerEntityConfigurations : IEntityTypeConfiguration<SongPerformer>
+    {
+        public void Configure(EntityTypeBuilder<SongPerformer> entity)
+        {
+            entity
+                .HasKey(sp => new { sp.PerformerId, sp.SongId });
+
+            //Relations
+
+            entity
+                .HasOne(sp => sp.Performer)
+                .WithMany(p => p.PerformerSongs)
+                .HasForeignKey(sp => sp.PerformerId);
+
+            entity
+                .HasOne(sp => sp.Song)
+                .WithMany(s => s.SongPerformers)
+                .HasForeignKey(sp => sp.SongId);
+
+                
+        }
+    }
+}
