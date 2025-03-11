a138b816bab1704dc10994fb3f6b67cb04e3f198
Georgi Donchev <booyaow24@gmail.com>
2025-03-11 21:30:45 +0000

    Added ProducerEntityConfigurations

---
+++ Entity Framework Core/LINQ/MusicHub/Data/Configurations/ProducerEntityConfigurations.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MusicHub.Data.Models;
+using static MusicHub.Data.Models.EntityValidationConstants.ProducerConstants;
+
+namespace MusicHub.Data.Configurations
+{
+    public class ProducerEntityConfigurations : IEntityTypeConfiguration<Producer>
+    {
+        public void Configure(EntityTypeBuilder<Producer> entity)
+        {
+            entity
+                .HasKey(p => p.Id);
+
+            entity
+                .Property(p => p.Name)
+                .IsRequired()
+                .IsUnicode()
+                .HasMaxLength(ProducerNameMaxLength);
+
+            entity
+                .Property(p => p.Pseudonym)
+                .IsRequired(false);
+
+            entity
+                .Property(p => p.PhoneNumber)
+                .IsRequired(false);
+
+        }
+    }
+}
