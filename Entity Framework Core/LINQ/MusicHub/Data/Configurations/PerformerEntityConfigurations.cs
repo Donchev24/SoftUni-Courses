4505af8af10b08f887c93c241eaf4ca0d043266e
Georgi Donchev <booyaow24@gmail.com>
2025-03-11 21:30:17 +0000

    Added PerformerEntity Configurations

---
+++ Entity Framework Core/LINQ/MusicHub/Data/Configurations/PerformerEntityConfigurations.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MusicHub.Data.Models;
+using static MusicHub.Data.Models.EntityValidationConstants.PerformerConstants;
+
+namespace MusicHub.Data.Configurations
+{
+    public class PerformerEntityConfigurations : IEntityTypeConfiguration<Performer>
+    {
+        public void Configure(EntityTypeBuilder<Performer> entity)
+        {
+            entity
+                .HasKey(p => p.Id);
+
+            entity
+                .Property(p => p.FirstName)
+                .IsRequired()
+                .IsUnicode()
+                .HasMaxLength(FirstNameMaxLength);
+
+            entity
+                .Property(p => p.LastName)
+                .IsRequired()
+                .IsUnicode()
+                .HasMaxLength(LastNameMaxLength);
+
+            entity
+                .Property(p => p.Age)
+                .IsRequired();
+
+            entity
+               .Property(p => p.NetWorth)
+               .IsRequired();
+        }
+    }
+}
