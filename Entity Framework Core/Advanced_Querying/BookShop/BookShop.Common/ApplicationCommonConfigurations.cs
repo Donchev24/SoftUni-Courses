87a3efcfb279e94853660a3dbe20b62bdae555c8
Georgi Donchev <booyaow24@gmail.com>
2025-03-15 12:05:10 +0000

    Added DB Context And Configurations

---
+++ Entity Framework Core/Advanced_Querying/BookShop/BookShop.Common/ApplicationCommonConfigurations.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Common
+{
+    public static class ApplicationCommonConfigurations
+    {
+        public const string ConnectionString =
+             "Server=.;Database=BookShop;Integrated Security=True;";
+    }
+}
