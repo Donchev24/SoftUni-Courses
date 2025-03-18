3941d642bd55dcfc0d8603d7ab6e56776702e43d
Georgi Donchev <booyaow24@gmail.com>
2025-03-18 22:09:19 +0000

    Added Initial Migration

--- Entity Framework Core/JavaScript_Object_Notation-JSON/ProductShop/ProductShop/StartUp.cs
+++ Entity Framework Core/JavaScript_Object_Notation-JSON/ProductShop/ProductShop/StartUp.cs
@@ -1,9 +1,12 @@
-﻿namespace ProductShop
+﻿using ProductShop.Data;
+
+namespace ProductShop
 {
     public class StartUp
     {
         public static void Main()
         {
+            using ProductShopContext dbContext = new ProductShopContext();
         }
     }
 }
