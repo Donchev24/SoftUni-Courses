4c929aae22e6ec56e82a32e2b1c5f457020ea87b
Georgi Donchev <booyaow24@gmail.com>
2025-04-01 19:44:02 +0100

    Added ImportSupplier Exercise Method

---
+++ Entity Framework Core/JavaScript_Object_Notation-JSON/CarDealer/CarDealer/DTOs/Import/ImportSupplierDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealer.DTOs.Import
+{
+    public class ImportSupplierDto
+    {
+        public string Name { get; set; } = null!;
+
+        public string isImporter { get; set; } = null!;
+
+    }
+}
--- Entity Framework Core/JavaScript_Object_Notation-JSON/CarDealer/CarDealer/StartUp.cs
+++ Entity Framework Core/JavaScript_Object_Notation-JSON/CarDealer/CarDealer/StartUp.cs
@@ -1,10 +1,75 @@
-﻿namespace CarDealer
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
+            CarDealerContext dbContext = new CarDealerContext();
 
+            string inputJson = File.ReadAllText("../../../Datasets/suppliers.json");
+
+            string result = ImportSuppliers(dbContext, inputJson);
+
+            Console.WriteLine(result);
+        }
+
+        public static string ImportSuppliers(CarDealerContext dbContext, string inputJson)
+        {
+            string result = string.Empty;
+            ICollection<Supplier> suppliers = new List<Supplier>();
+
+            ImportSupplierDto[]? supplierDtos = JsonConvert
+                .DeserializeObject<ImportSupplierDto[]>(inputJson);
+
+            if (supplierDtos != null)
+            {
+                foreach (ImportSupplierDto supplierDto in supplierDtos)
+                {
+                    if (!IsValid(supplierDto))
+                    {
+                        continue;
+                    }
+
+                    bool isImport = false;
+
+                    if (supplierDto.isImporter == "true")
+                    {
+                        isImport = true;
+                    }
+
+                    Supplier supplier = new Supplier()
+                    {
+                        Name = supplierDto.Name,
+                        IsImporter = isImport
+                    };
+
+                    suppliers.Add(supplier);
+                }
+
+                dbContext.AddRange(suppliers);
+                dbContext.SaveChanges();
+
+                result = $"Successfully imported {suppliers.Count}.";
+            }
+            return result;
+        }
+
+        public static bool IsValid(object dto)
+        {
+            var validateContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            bool isValid = Validator
+                .TryValidateObject(dto, validateContext, validationResult, true);
+
+            return isValid;
         }
     }
 }
