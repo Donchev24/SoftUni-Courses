12a9c42c7c9b0e9e245226a5c8b3b5219778724d
Georgi Donchev <booyaow24@gmail.com>
2025-04-06 20:26:51 +0100

    Added New Lesson And Problem Description doc

---
+++ Entity Framework Core/Extensible_Markup_Language-XML/CarDealer/CarDealer/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using CarDealer.Data;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            public static void Main()
+            {
+              
+            }
+
+        }
+        
+    }
+}
