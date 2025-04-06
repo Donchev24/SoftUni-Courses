37e83ec459b09b74e7e21d91bf409c07a22f37e2
Georgi Donchev <booyaow24@gmail.com>
2025-04-06 21:54:43 +0100

    Added SuppliersImport Exercise Method

--- Entity Framework Core/Extensible_Markup_Language-XML/CarDealer/CarDealer/StartUp.cs
+++ Entity Framework Core/Extensible_Markup_Language-XML/CarDealer/CarDealer/StartUp.cs
@@ -1,5 +1,7 @@
 ﻿using CarDealer.Data;
+using CarDealer.DTOs.Import;
 using CarDealer.Models;
+using CarDealer.Utilities;
 using System.ComponentModel.DataAnnotations;
 
 namespace CarDealer
@@ -9,11 +11,53 @@ namespace CarDealer
         public static void Main()
         {
             using CarDealerContext dbContext = new CarDealerContext();
+
+            string xmlFilePath = "../../../Datasets/suppliers.xml";
+            string inputXml = File.ReadAllText(xmlFilePath);
+
+            string result = ImportSuppliers(dbContext, inputXml);
+            Console.WriteLine(result);
         }
 
         public static string ImportSuppliers(CarDealerContext context, string inputXml)
         {
+            string result = string.Empty;
 
+            ImportSupplierDto[]? supplierDtos = XmlHelper
+                .Deserialize<ImportSupplierDto[]>(inputXml, "Suppliers");
+
+            if (supplierDtos != null)
+            {
+                ICollection<Supplier> validSuppliers = new List<Supplier>();
+
+                foreach (ImportSupplierDto supplierDto in supplierDtos)
+                {
+                    if (!IsValid(supplierDto))
+                    {
+                        continue;
+                    }
+
+                    bool isImporterValid = bool.TryParse(supplierDto.IsImporter, out bool isImporter);
+                    if (!isImporterValid)
+                    {
+                        continue;
+                    }
+
+                    Supplier supplier = new Supplier()
+                    {
+                        Name = supplierDto.Name,
+                        IsImporter = isImporter
+                    };
+
+                    validSuppliers.Add(supplier);
+
+                }
+
+                context.AddRange(validSuppliers);
+                context.SaveChanges();
+                result = $"Successfully imported {validSuppliers.Count}";
+            }
+            return result ;
         }
 
         public static bool IsValid(object dto)
