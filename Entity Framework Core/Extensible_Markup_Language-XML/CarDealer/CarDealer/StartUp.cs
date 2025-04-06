688f7059e40c7a5cf8bff5edc79ee989772f5b12
Georgi Donchev <booyaow24@gmail.com>
2025-04-06 21:06:39 +0100

    Added InitialMigration

--- Entity Framework Core/Extensible_Markup_Language-XML/CarDealer/CarDealer/StartUp.cs
+++ Entity Framework Core/Extensible_Markup_Language-XML/CarDealer/CarDealer/StartUp.cs
@@ -1,5 +1,6 @@
 ﻿using CarDealer.Data;
 using CarDealer.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace CarDealer
 {
@@ -7,12 +8,25 @@ namespace CarDealer
     {
         public static void Main()
         {
-            public static void Main()
-            {
-              
-            }
+            using CarDealerContext dbContext = new CarDealerContext();
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
 
         }
-        
+
+        public static bool IsValid(object dto)
+        {
+            var validateContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            bool isValid = Validator
+                .TryValidateObject(dto, validateContext, validationResult, true);
+
+            return isValid;
+        }
+
+
     }
 }
