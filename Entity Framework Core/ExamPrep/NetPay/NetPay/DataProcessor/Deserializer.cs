7456c6f1c25d8403eac515e80f890d24618e71f6
Georgi Donchev <booyaow24@gmail.com>
2025-04-12 17:05:43 +0100

    Added ImportHouseholds Exercise Method

--- Entity Framework Core/ExamPrep/NetPay/NetPay/DataProcessor/Deserializer.cs
+++ Entity Framework Core/ExamPrep/NetPay/NetPay/DataProcessor/Deserializer.cs
@@ -1,5 +1,9 @@
 ﻿using NetPay.Data;
+using NetPay.Data.Models;
+using NetPay.Data.Utilities;
+using NetPay.DataProcessor.ImportDtos;
 using System.ComponentModel.DataAnnotations;
+using System.Text;
 
 namespace NetPay.DataProcessor
 {
@@ -12,10 +16,63 @@ namespace NetPay.DataProcessor
 
         public static string ImportHouseholds(NetPayContext context, string xmlString)
         {
-            throw new NotImplementedException();
+            ICollection<Household> households = new List<Household>();
+
+            StringBuilder output = new StringBuilder();
+
+            ImportHouseholdDto[]? householdDtos = XmlHelper
+                .Deserialize<ImportHouseholdDto[]>(xmlString, "Households");
+
+
+            if (householdDtos != null && householdDtos.Length > 0)
+            {
+                foreach (ImportHouseholdDto householdDto in householdDtos)
+                {
+                    if (!IsValid(householdDto))
+                    {
+                        output.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    bool isDuplicate = households
+                        .Any(h => h.ContactPerson == householdDto.ContactPerson 
+                               || h.Email == householdDto.Email
+                               || h.PhoneNumber == householdDto.PhoneNumber
+                        );
+
+                    //bool isExistInDb = context
+                    //    .Households
+                    //    .Any(h => h.ContactPerson == householdDto.ContactPerson
+                    //           || h.Email == householdDto.Email
+                    //           || h.PhoneNumber == householdDto.PhoneNumber);
+
+                    if (isDuplicate)
+                    {
+                        output.AppendLine(DuplicationDataMessage);
+                        continue;
+                    }
+
+                    Household household = new Household()
+                    {
+                        ContactPerson = householdDto.ContactPerson,
+                        Email = householdDto.Email,
+                        PhoneNumber = householdDto.PhoneNumber
+                    };
+
+                    households.Add(household);
+
+                    string toAppend = String.Format(SuccessfullyImportedHousehold, householdDto.ContactPerson);
+                    output.AppendLine(toAppend);
+                }
+
+                context.Households.AddRange(households);
+                context.SaveChanges();
+            }
+
+            return output.ToString().TrimEnd();
         }
 
-        public static string ImportExpenses(NetPayContext context, string jsonString)
+        public static string ImportExpenses(NetPayContext context, string jsonString) //////////
         {
             throw new NotImplementedException();
         }
---
+++ Entity Framework Core/ExamPrep/NetPay/NetPay/DataProcessor/ImportDtos/ImportHouseholdDto.cs
@@ -0,0 +1,33 @@
+﻿using NetPay.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace NetPay.DataProcessor.ImportDtos
+{
+    [XmlType("Household")]
+    public class ImportHouseholdDto
+    {
+        [Required]
+        [XmlElement("ContactPerson")]
+        [MinLength(5)]
+        [MaxLength(50)]
+        public string ContactPerson { get; set; } = null!;
+
+        [XmlElement("Email")]
+        [MinLength(6)]
+        [MaxLength(80)]
+        public string? Email { get; set; }
+
+        [Required]
+        [XmlAttribute("phone")]
+        [MinLength(15)]
+        [MaxLength(15)]
+        [RegularExpression(@"^\+\d{3}/\d{3}-\d{6}$")]
+        public string PhoneNumber { get; set; } = null!;
+    }
+}
