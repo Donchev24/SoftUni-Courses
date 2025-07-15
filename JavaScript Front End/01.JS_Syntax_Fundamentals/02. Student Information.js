7caae9e3e595244c988b5d1141f783a56df6dd34
Georgi Donchev <booyaow24@gmail.com>
2025-07-15 15:27:06 +0300

    Added Exercise

---
+++ JavaScript Front End/01.JS_Syntax_Fundamentals/02. Student Information.js
@@ -0,0 +1,6 @@
+function printStudentInfo (studentName, age, avgGrade) {
+    console.log(`Name: ${studentName}, Age: ${age}, Grade: ${avgGrade.toFixed(2)}`);
+    
+}
+
+printStudentInfo('John', 15, 5.54678);
