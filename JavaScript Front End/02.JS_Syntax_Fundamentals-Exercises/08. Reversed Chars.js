a153d3e5deac0fef55fee2dc99e72dad000fa264
Georgi Donchev <booyaow24@gmail.com>
2025-07-24 14:30:12 +0300

    Added Exercise

---
+++ JavaScript Front End/02.JS_Syntax_Fundamentals-Exercises/08. Reversed Chars.js
@@ -0,0 +1,9 @@
+function reversedPrint(firstChar, secondChar, thridChar) {
+    
+    let newStr = thridChar + ` ` + secondChar + ` ` + firstChar;
+    console.log(newStr);
+}
+
+reversedPrint('A',
+'B',
+'C');
