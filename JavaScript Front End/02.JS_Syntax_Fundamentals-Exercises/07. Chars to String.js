664b9a64e248a09abcc6e66605c7cedaa7ae9412
Georgi Donchev <booyaow24@gmail.com>
2025-07-24 14:25:20 +0300

    Added Exercise

---
+++ JavaScript Front End/02.JS_Syntax_Fundamentals-Exercises/07. Chars to String.js
@@ -0,0 +1,9 @@
+function charsToString(firstChar, secondChar, thridChar) {
+    
+    let newStr = firstChar + secondChar + thridChar;
+    console.log(newStr);
+}
+
+charsToString('1',
+'5',
+'p');
