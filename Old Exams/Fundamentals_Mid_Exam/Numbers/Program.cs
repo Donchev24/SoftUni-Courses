08844c6343b18abfbca5f7bad5d1c4f465e23b46
Georgi Donchev <booyaow24@gmail.com>
2025-09-02 12:30:45 +0300

    Added an exercise

---
+++ Old Exams/Fundamentals_Mid_Exam/Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> values = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> result = values
+                .Where(x => x > values
+                .Average())
+                .OrderByDescending(x => x)
+                .Take(5)
+                .ToList();
+
+            if (result.Count == 0)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", result));
+            }
+        }
+    }
+}
