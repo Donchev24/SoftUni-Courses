38081febcd73c31d06dc914e6cfd0f198c81cdb1
Georgi Donchev <booyaow24@gmail.com>
2025-09-02 12:24:10 +0300

    Added an exercise

---
+++ Old Exams/Basic_Exam/Football_Results/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Football_Results
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 3;
+            int cols = 2;
+
+            int[,] scores = new int[rows,cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] result = Console.ReadLine().Split(":").Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    scores[row, col] = result[col];
+                }
+            }
+
+            int wins = 0;
+            int loses = 0;
+            int draws = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < 1; col++)
+                {
+                    if (scores[row, col] > scores[row, col + 1])
+                    {
+                        wins++;
+                    }
+                    else if (scores[row, col] < scores[row, col + 1])
+                    {
+                        loses++;
+                    }
+                    else
+                    {
+                        draws++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Team won {wins} games.");
+            Console.WriteLine($"Team lost {loses} games.");
+            Console.WriteLine($"Drawn games: {draws}");
+        }
+    }
+}
