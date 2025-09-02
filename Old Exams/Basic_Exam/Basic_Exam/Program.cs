7bca8c7885ca80670d50c1234f67547e938aa715
Georgi Donchev <booyaow24@gmail.com>
2025-09-02 12:22:55 +0300

    Added an exercise

---
+++ Old Exams/Basic_Exam/Basic_Exam/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Basic_Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double singleRocketPrice = double.Parse(Console.ReadLine());
+            int rocketsCount = int.Parse(Console.ReadLine());
+            int shoesCount = int.Parse(Console.ReadLine());
+
+            double singleShoesPrice = singleRocketPrice / 6;
+
+            double totalRocketsPrice = singleRocketPrice * rocketsCount;
+            double totalShoesPrice = singleShoesPrice * shoesCount;
+            double otherGearPrice = (totalRocketsPrice + totalShoesPrice) * 0.2;
+            
+            double totalPrice = totalRocketsPrice + totalShoesPrice + otherGearPrice;
+
+            double oneEihthOfPrice = totalPrice / 8;
+
+            Console.WriteLine($"Price to be paid by Djokovic {Math.Floor(oneEihthOfPrice)}");
+            Console.WriteLine($"Price to be paid by sponsors {Math.Ceiling(oneEihthOfPrice * 7)}");
+        }
+    }
+}
