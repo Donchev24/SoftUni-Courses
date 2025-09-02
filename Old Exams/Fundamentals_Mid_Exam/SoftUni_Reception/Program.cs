d006958aea5f95c17221d75d0aa618793c47b7ca
Georgi Donchev <booyaow24@gmail.com>
2025-09-02 12:31:21 +0300

    Added an exercise

---
+++ Old Exams/Fundamentals_Mid_Exam/SoftUni_Reception/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SoftUni_Reception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmployeeEfficiency = int.Parse(Console.ReadLine());
+            int secondEmployeeEfficiency = int.Parse(Console.ReadLine());
+            int thirdEmployeeEfficiency = int.Parse(Console.ReadLine());
+
+            int studentsCount = int.Parse(Console.ReadLine());
+
+            int customersCapacityPerHour = firstEmployeeEfficiency + secondEmployeeEfficiency + thirdEmployeeEfficiency;
+
+            int hoursNeed = 0;
+            int hoursWorkedWithoutBreak = 0;
+
+            if (studentsCount > 0)
+            {
+                while (studentsCount > 0)
+                {
+                    if (hoursWorkedWithoutBreak > 0 && hoursWorkedWithoutBreak % 3 == 0)
+                    {
+                        hoursNeed++;
+                        hoursWorkedWithoutBreak = 0;
+                    }
+                    studentsCount -= customersCapacityPerHour;
+                    hoursNeed++;
+                    hoursWorkedWithoutBreak++;
+                }
+            }
+
+            Console.WriteLine($"Time needed: {hoursNeed}h.");
+        }
+    }
+}
