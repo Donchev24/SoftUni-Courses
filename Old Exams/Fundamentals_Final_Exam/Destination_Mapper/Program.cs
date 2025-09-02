4bd92c6470219a2bc4629062f112a121690914f1
Georgi Donchev <booyaow24@gmail.com>
2025-09-02 13:08:15 +0300

    Added a new exercise

---
+++ Old Exams/Fundamentals_Final_Exam/Destination_Mapper/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Destination_Mapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"([=/])([A-Z][a-zA-Z]{2,})\1";
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            List<string> destinations = new List<string>();
+            int travelPoints = 0;
+
+            foreach (Match match in matches)
+            {
+                string place = match.Groups[2].Value;
+                destinations.Add(place);
+                travelPoints += place.Length;
+            }
+
+            Console.WriteLine("Destinations: " + (destinations.Count > 0 ? string.Join(", ", destinations) : ""));
+            Console.WriteLine("Travel Points: " + travelPoints);
+        }
+    }
+}
