202328a2974ca81f33857d0edbad5f0b2f0ab59d
Georgi Donchev <booyaow24@gmail.com>
2025-09-08 21:32:26 +0300

    Added an old exam and the first problem solution along with the problem description

---
+++ Old Exams/Fundamentals_Mid_Exam/Array_Modifier/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace Array_Modifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                if (command == "decrease")
+                {
+                    for (int i = 0; i < values.Length; i++)
+                    {
+                        values[i]--;
+                    }
+                }
+                else
+                {
+                    string[] tokens = command.Split().ToArray();
+
+                    string action = tokens[0];
+                    int firstIndex = int.Parse(tokens[1]);
+                    int secondIndex = int.Parse(tokens[2]);
+
+                    if (action == "swap")
+                    {
+                        int temporarySavedValue = values[secondIndex];
+                        values[secondIndex] = values[firstIndex];
+                        values[firstIndex] = temporarySavedValue;
+                    }
+                    else if (action == "multiply")
+                    {
+                        values[firstIndex] *= values[secondIndex];
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", values));
+        }
+    }
+}
